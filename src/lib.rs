7e80958db6e8b880af7dd7961048abd1a602368a
Ulysse Beaugnon <ulysse.beaugnon@ens.fr>
2018-03-25 17:45:08 +0200

    use the new thread counter in model

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,6 @@ pub mod model;
 pub mod search_space;
 
 // FIXME: thread-mapping
-// - have a quotient set to list active thread dims
 // - rely on thread-mapping for the dim maps
 // - put constraints on the threads quotient rather than individual instructions
 // - use the thread mapping in codegen
