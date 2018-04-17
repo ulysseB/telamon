b3492c64915064814200ab0da40d7d2fd9ef5a4d
Nicolas Tollenaere <nicolas.tollenaere@inria.com>
2018-04-17 09:40:36 -0400

    Instruction printer

--- src/device/x86/printer.rs
+++ src/device/x86/printer.rs
@@ -39,11 +39,52 @@ fn param_decl(param: &ParamVal, namer: &NameMap) -> String {
         )
 }
 
+/// Prints an instruction.
+fn inst(inst: &Instruction, namer: &mut NameMap, fun: &Function) -> String {
+    let assignement = format!("{} =", namer.name_inst(inst).to_string());
+    match *inst.operator() {
+        op::Add(ref lhs, ref rhs, _) => {
+            format!("{} {} + {};", assignement, namer.name_op(lhs), namer.name_op(rhs))
+        },
+        op::Sub(ref lhs, ref rhs, _) => {
+            format!("{} {} - {};", assignement, namer.name_op(lhs), namer.name_op(rhs))
+        },
+        op::Mul(ref lhs, ref rhs, _, return_type) => {
+            format!("{} {} * {};", assignement, namer.name_op(lhs), namer.name_op(rhs))
+        },
+        op::Mad(ref mul_lhs, ref mul_rhs, ref add_rhs, _ing) => {
+            format!("{} {} * {} + {};", assignement, namer.name_op(mul_lhs), namer.name_op(mul_rhs), namer.name_op(add_rhs))
+        },
+        op::Div(ref lhs, ref rhs, _) => {
+            format!("{} {} / {};", assignement, namer.name_op(lhs), namer.name_op(rhs))
+        },
+        op::Mov(ref op) => {
+            format!("{} {};", assignement, namer.name_op(op).to_string())
+        },
+        op::Ld(ld_type, ref addr, _) => {
+            format!("{} *({} *){}", assignement, cpu_type(&ld_type), namer.name_op(addr))
+        },
+        op::St(ref addr, ref val, _,  _) => {
+            format!("*({} *){} = {}", 
+                    cpu_type(&addr.t()), 
+                    namer.name_op(addr),
+                    namer.name_op(val).to_string())
+        },
+        op::Cast(ref op, t) => {
+            format!("{} ({}) {}", assignement, cpu_type(&t), namer.name_op(op))
+        },
+        op::TmpLd(..) | op::TmpSt(..) => panic!("non-printable instruction")
+    }
+}
+
 fn cpu_loop(fun: &Function, dim: &Dimension, cfgs: &[Cfg], namer: &mut NameMap)
     -> String
 {
     match dim.kind() {
-        DimKind::LOOP => String::from("goto "),
-        _ => String::from("")
+        DimKind::LOOP => {
+            let idx = namer.name_index(dim.id()).to_string();
+            String::from("goto ")
+        }
+        _ => { String::from("") }
     }
 }
