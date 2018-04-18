7c5c3fcd4d08ebf03a39140e70548680ee84f236
adjivas <adjivas@users.noreply.github.com>
2018-04-18 18:34:59 +0200

    Enumeration CmpOp

--- telamon-gen/src/lexer/ffi.rs
+++ telamon-gen/src/lexer/ffi.rs
@@ -4,9 +4,72 @@ use ::libc;
 pub type YyScan = *const libc::c_void;
 pub type YyBufferState = *const libc::c_void;
 
+#[derive(Copy, Clone)]
 #[repr(C)]
 pub union YyLval {
     val: libc::c_int,
+    pub cmp_op: CmpOp,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub enum YyToken {
+    ValueIdent,
+    ChoiceIdent,
+    Var,
+    Doc,
+    CmpOp,
+    InvalidToken,
+    Code,
+    CounterKind,
+    Bool,
+    CounterVisibility,
+    And,
+    Trigger,
+    When,
+    Alias,
+    Counter,
+    Define,
+    Enum,
+    Equal,
+    Forall,
+    In,
+    Is,
+    Not,
+    Require,
+    Requires,
+    Value,
+    End,
+    Symmetric,
+    AntiSymmetric,
+    Arrow,
+    Colon,
+    Comma,
+    LParen,
+    RParen,
+    BitOr,
+    Or,
+    SetDefkey,
+    Set,
+    SubsetOf,
+    SetIdent,
+    Base,
+    Disjoint,
+    Quotient,
+    Of,
+    Divide,
+    EOF = libc::EOF as _,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[repr(C)]
+pub enum CmpOp {
+    Lt,
+    Gt,
+    Leq,
+    Geq,
+    Eq,
+    Neq,
 }
 
 extern {
@@ -14,7 +77,7 @@ extern {
 
     pub fn yylex_init(scanner: *const YyScan) -> libc::c_int;
     pub fn yy_scan_string(yy_str: *const libc::c_char, yyscanner: YyScan) -> YyBufferState;
-    pub fn yylex(yyscanner: YyScan) -> libc::c_int;
+    pub fn yylex(yyscanner: YyScan) -> YyToken;
     pub fn yyget_text(yyscanner: YyScan) -> *const libc::c_char;
     pub fn yy_delete_buffer(b: YyBufferState, yyscanner: YyScan);
     pub fn yylex_destroy(yyscanner: YyScan) -> libc::c_int;
--- telamon-gen/src/lexer/mod.rs
+++ telamon-gen/src/lexer/mod.rs
@@ -3,14 +3,28 @@
 mod ffi;
 mod token;
 
+use ir;
 use std::{io,ptr};
 
-use ::libc;
 pub use self::token::Token;
 
+use self::ffi::{
+    YyScan,
+    YyBufferState,
+    yylex_init,
+    yy_scan_string,
+    yy_delete_buffer,
+    yylex_destroy,
+    yylex,
+    YyToken,
+    yylval,
+
+    CmpOp
+};
+
 pub struct Lexer {
-    scanner: ffi::YyScan,
-    buffer: ffi::YyBufferState,
+    scanner: YyScan,
+    buffer: YyBufferState,
 }
 
 impl Lexer {
@@ -19,12 +33,12 @@ impl Lexer {
 
         input.read_to_end(&mut buffer);
         unsafe {
-            let scanner: ffi::YyScan = ptr::null();
+            let scanner: YyScan = ptr::null();
 
-            ffi::yylex_init(&scanner); // https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
+            yylex_init(&scanner); // https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
             Lexer {
                 scanner: scanner,
-                buffer: ffi::yy_scan_string(buffer.as_ptr() as *const _, scanner), // https://westes.github.io/flex/manual/Multiple-Input-Buffers.html
+                buffer: yy_scan_string(buffer.as_ptr() as *const _, scanner), // https://westes.github.io/flex/manual/Multiple-Input-Buffers.html
             }
         }
     }
@@ -33,9 +47,8 @@ impl Lexer {
 impl Drop for Lexer {
     fn drop(&mut self) {
         unsafe {
-                libc::write(1, b"\n".as_ptr() as *const _, 1);
-            ffi::yy_delete_buffer(self.buffer, self.scanner); // https://westes.github.io/flex/manual/Multiple-Input-Buffers.html
-            ffi::yylex_destroy(self.scanner); // https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
+            yy_delete_buffer(self.buffer, self.scanner); // https://westes.github.io/flex/manual/Multiple-Input-Buffers.html
+            yylex_destroy(self.scanner); // https://westes.github.io/flex/manual/Init-and-Destroy-Functions.html#index-yylex_005finit
         }
     }
 }   
@@ -44,6 +57,60 @@ impl Iterator for Lexer {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        unsafe {
+            match yylex(self.scanner) {
+                YyToken::EOF => None,
+                YyToken::Alias => Some(Token::Alias),
+                YyToken::Counter => Some(Token::Counter),
+                YyToken::Define => Some(Token::Define),
+                YyToken::Enum => Some(Token::Enum),
+                YyToken::Forall => Some(Token::Forall),
+                YyToken::In => Some(Token::In),
+                YyToken::Is => Some(Token::Is),
+                YyToken::Not => Some(Token::Not),
+//                YyToken::Product => Some(Token::Product),
+//                YyToken::Require => Some(Token::RequireKind),
+                YyToken::Requires => Some(Token::Requires),
+//              YyToken::Sum => Some(Token::RequireKind),
+                YyToken::Value => Some(Token::Value),
+                YyToken::When => Some(Token::When),
+                YyToken::Trigger => Some(Token::Trigger),
+//                YyToken::Half => Some(Token::CounterVisibility),
+//                YyToken::HIDDEN => Some(Token::CounterVisibility),
+                YyToken::Base => Some(Token::Base),
+                YyToken::Set => Some(Token::Set),
+                YyToken::SubsetOf => Some(Token::SubsetOf),
+                YyToken::Disjoint => Some(Token::Disjoint),
+                YyToken::Quotient => Some(Token::Quotient),
+                YyToken::Of => Some(Token::Of),
+
+                YyToken::Colon => Some(Token::Colon),
+                YyToken::Comma => Some(Token::Comma),
+                YyToken::LParen => Some(Token::LParen),
+                YyToken::RParen => Some(Token::RParen),
+                YyToken::BitOr => Some(Token::BitOr),
+                YyToken::Or => Some(Token::Or),
+                YyToken::And => Some(Token::And),
+                YyToken::CmpOp => {
+                    match yylval.cmp_op {
+                        CmpOp::Gt => Some(Token::CmpOp(ir::CmpOp::Gt)),
+                        CmpOp::Lt => Some(Token::CmpOp(ir::CmpOp::Lt)),
+                        CmpOp::Geq => Some(Token::CmpOp(ir::CmpOp::Geq)),
+                        CmpOp::Leq => Some(Token::CmpOp(ir::CmpOp::Leq)),
+                        _ => None,
+                    }
+                },
+//                YyToken::Equals => Some(Token::CmpOp(ir::CmpOp::Eq)),
+//                YyToken::NotEquals => Some(Token::CmpOp(ir::CmpOp::Neq)),
+                YyToken::Equal => Some(Token::Equal),
+//                YyToken::Doc => Some(Token::Doc),
+                YyToken::End => Some(Token::End),
+                YyToken::Symmetric => Some(Token::Symmetric),
+                YyToken::AntiSymmetric => Some(Token::AntiSymmetric),
+                YyToken::Arrow => Some(Token::Arrow),
+                YyToken::Divide => Some(Token::Divide),
+                _ => unimplemented!(),
+            }
+        }
     }
 }
