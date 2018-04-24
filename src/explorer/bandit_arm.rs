425de41cb6f1a92a698f6067d86c51254415045c
Ulysse Beaugnon <ulysse.beaugnon@ens.fr>
2018-04-24 10:17:40 -0400

    revamp ascend

--- src/explorer/bandit_arm.rs
+++ src/explorer/bandit_arm.rs
@@ -29,10 +29,37 @@ impl<'a, 'b> Tree<'a, 'b> {
             config,
         }
     }
+
+    /// Removes the dead ends along the given path. Assumes the path points to a dead-end.
+    /// Updates bounds along the way.
+    fn clean_deadends(&self, mut path: Path<'a>, cut: f64) {
+        // A `None` bound indicates the path points to a dead-end.
+        let mut bound = None;
+        while let Some((node, pos)) = path.0.pop() {
+            if let Some(node) = node.upgrade() {
+                let mut lock = unwrap!(node.write());
+                if let Some(bound) = bound {
+                    lock.children[pos].update_bound(bound);
+                    // If the bound was set, then we finished removing deadends.
+                    return;
+                } else {
+                    lock.children[pos] = SubTree::Empty;
+                    bound = lock.bound();
+                    // Children with a bound above the cut are considered as dead-ends.
+                    if bound.map(|b| b >= cut).unwrap_or(false) { bound = None; }
+                }
+            } else { return; }
+        }
+        // If we did not returned before, we have reached the root of the tree.
+        let mut lock = unwrap!(self.shared_tree.write());
+        if let Some(bound) = bound { lock.update_bound(bound); } else {
+            *lock = SubTree::Empty
+        }
+    }
 }
 
 impl<'a, 'b> Store<'a> for Tree<'a, 'b> {
-    type PayLoad = Payload<'a>;
+    type PayLoad = Path<'a>;
 
     fn update_cut(&self, new_cut: f64) {
         *unwrap!(self.cut.write()) = new_cut;
@@ -47,13 +74,12 @@ impl<'a, 'b> Store<'a> for Tree<'a, 'b> {
         info!("trimming finished");
     }
 
-    fn commit_evaluation(&self, mut payload: Self::PayLoad, eval: f64) {
-        // FIXME: >>>>>>>>>>>>
-        let parent_opt = payload.path.pop();
-        if let Some((weak_node, pos)) = parent_opt {
-            if let Some(node_lock) = weak_node.upgrade() {
-                iter_ascend(node_lock, eval, pos, payload.leaf_pos, &mut payload.path,
-                            payload.is_complete)
+    fn commit_evaluation(&self, mut path: Self::PayLoad, eval: f64) {
+        while let Some((node, idx)) = path.0.pop() {
+            if let Some(node) = node.upgrade() {
+                if !unwrap!(node.write()).update_rewards(&self.config, idx, eval) {
+                    return;
+                }
             }
         }
     }
@@ -75,16 +101,16 @@ impl<'a, 'b> Store<'a> for Tree<'a, 'b> {
                     match iter_descend(self.config, context, node, cut) {
                         // FIXME: get rid of OldDescendResult
                         OldDescendResult::Finished => { return None; }
-                        OldDescendResult::DeadEnd(pos, parent_stack) => {
-                            // FIXME: This remove the dead branch, ensure it is not called from montecarlo
-                            thread_ascend_tree_no_val(pos, parent_stack);
+                        OldDescendResult::DeadEnd(path) => {
+                            self.clean_deadends(path, cut);
                         }
-                        OldDescendResult::Leaf(cand, leaf_pos, path) => {
-                            return Some((cand, Payload { path, leaf_pos, is_complete: true }));
+                        OldDescendResult::Leaf(cand, path) => {
+                            self.clean_deadends(path.clone(), cut);
+                            return Some((cand, path));
                         }
-                        OldDescendResult::MonteCarloLeaf(cand, leaf_pos, path) => {
-                            // FIXME: ensure we return complete if the montecarlo has 0 levels
-                            return Some((cand, Payload { path, leaf_pos, is_complete: false }));
+                        OldDescendResult::MonteCarloLeaf(cand, path) => {
+                            // FIXME: ensure we cleanup deadends if the montecarlo has 0 levels
+                            return Some((cand, path));
                         }
                         // We have no information on where and how the search fail, so we can not
                         // update the tree in any way.
@@ -97,16 +123,10 @@ impl<'a, 'b> Store<'a> for Tree<'a, 'b> {
 }
 
 
-/// Transmits the information needed to update the tree after an update.
-pub struct Payload<'a> {
-    /// Path in the tree that lead to the evaluated branch.
-    path: NodeStack<'a>,
-    /// Indicates the position of the leaf in the last node.
-    leaf_pos: usize,
-    /// Indicates if the branch was fully evaluated and should be cut or not.
-    is_complete: bool,
-}
 
+/// Path to follow to reach a leaf in the tree.
+#[derive(Clone)]
+pub struct Path<'a>(Vec<(Weak<RwLock<Children<'a>>>, usize)>);
 
 /// The search tree that will be traversed
 enum SubTree<'a> {
@@ -173,6 +193,11 @@ impl<'a> SubTree<'a> {
             },
             SubTree::Empty => DescendState::DeadEnd,
         }
+    }
+
+    /// Update the bound registered in the node, if possible.
+    fn update_bound(&mut self, bound: f64) {
+        if let SubTree::InternalNode(_, ref mut old_bound) = *self { *old_bound = bound }
     }
 }
 
@@ -251,6 +276,37 @@ impl<'a> Children<'a> {
             }
         })
     }
+
+
+    /// Update a rewards list given a new value and the position where it was found
+    /// returns true if the value was inserted in the node.
+    fn update_rewards(&mut self, config: &BanditConfig, pos: usize, val: f64) -> bool {
+        if self.children[pos].is_empty() { return false; }
+        let total_trials = self.rewards.iter().map(|x| x.0.len()).sum::<usize>();
+        // If total trials is less than THRESHOLD, then we simply push our new value
+        // in the node where it was found.
+        if total_trials < config.threshold {
+            self.rewards[pos].0.push(val);
+            true
+        } else {
+            let (child, idx, max) = unwrap!(self.find_max_rewards());
+            if val < max {
+                self.rewards[child].0[idx] = val;
+                true
+            } else { false }
+        }
+    }
+
+    /// Returns the tuple `(child_index, eval_index, value)` containing the position and
+    /// the value of the biggest reward registered.
+    fn find_max_rewards(&self) -> Option<(usize, usize, f64)> {
+        self.rewards.iter().enumerate().flat_map(|(child_idx, rewards)| {
+            let max = rewards.0.iter().cloned().enumerate()
+                .max_by(|lhs, rhs| cmp_f64(lhs.1, rhs.1));
+            max.map(|(idx, value)| (child_idx, idx, value))
+        }).max_by(|x1, x2| cmp_f64(x1.2, x2.2))
+    }
+
 }
 
 // FIXME: >>>>>>>>>>>>>>>..
@@ -266,21 +322,19 @@ fn iter_descend<'a>(config: &BanditConfig,
                     cut: f64) -> OldDescendResult<'a> {
     let mut parent_stack = vec![];
     let mut search_node_lock = node_root;
-    let mut current_pos = None;
     loop {
         let next_node;
         {
             let mut search_node = search_node_lock.write().unwrap();
             if let Some((idx, state)) = search_node.descend(config, context, cut) {
                 let weak_ref = Arc::downgrade(&search_node_lock);
-                parent_stack.push((weak_ref, current_pos));
-                current_pos = Some(idx);
+                parent_stack.push((weak_ref, idx));
                 match state {
                     DescendState::DeadEnd => {
-                        return OldDescendResult::DeadEnd(idx, parent_stack)
+                        return OldDescendResult::DeadEnd(Path(parent_stack))
                     },
                     DescendState::Leaf(candidate) => {
-                        return OldDescendResult::Leaf(candidate, idx, parent_stack)
+                        return OldDescendResult::Leaf(candidate, Path(parent_stack))
                     },
                     DescendState::InternalNode(children, is_new) => {
                         if is_new && config.monte_carlo {
@@ -290,245 +344,43 @@ fn iter_descend<'a>(config: &BanditConfig,
                                 .start_montecarlo(config, context, cut);
                             if let Some(cand) = monte_cand_opt {
                                 return handle_montecarlo_descend(
-                                    config, context, cand, idx, cut, parent_stack);
+                                    config, context, cand, cut, Path(parent_stack));
                             } else { return OldDescendResult::FailedMonteCarlo; }
                         } else { next_node = children.clone(); }
                     }
                 }
             } else {
-                if let Some(pos) = current_pos {
-                    return OldDescendResult::DeadEnd(pos, parent_stack)
-                } else { return OldDescendResult::Finished }
+                return if parent_stack.is_empty() { OldDescendResult::Finished } else {
+                    OldDescendResult::DeadEnd(Path(parent_stack))
+                }
             }
         }
         search_node_lock = next_node;
     }
 }
 
-type NodeStack<'a> = Vec<(Weak<RwLock<Children<'a>>>, Option<usize>)>;
-
 // These types are used as return type for the functions traversing the tree
 pub enum OldDescendResult<'a> {
     Finished,
-    DeadEnd(usize, NodeStack<'a>),
-    Leaf(Candidate<'a>, usize, NodeStack<'a>),
-    MonteCarloLeaf(Candidate<'a>, usize, NodeStack<'a>),
+    DeadEnd(Path<'a>),
+    Leaf(Candidate<'a>, Path<'a>),
+    MonteCarloLeaf(Candidate<'a>, Path<'a>),
     FailedMonteCarlo,
 }
-
-pub enum NodeAscendResult<'a> {
-    Children(Arc<RwLock<Children<'a>>>, Option<usize>),
-    NodeNoVal(Arc<RwLock<Children<'a>>>, Option<usize>),
-    NodeNotCompleted(Arc<RwLock<Children<'a>>>, Option<usize>),
-    // FIXME: can merge some variants
-    InvalidParent,
-    TreeUpdated,
-    Root,
-}
-
-enum UpdateRes {
-    KeepGoing,
-    ValNotInserted,
-    TreeUpdated,
-}
-
-
 
 /// Handles the descend from a candidate and returns an appropriate OldDescendResult
 fn handle_montecarlo_descend<'a>(config: &BanditConfig,
                                  context: &Context,
                                  cand: Candidate<'a>,
-                                 pos: usize,
                                  cut: f64,
-                                 parent_stack: NodeStack<'a>) -> OldDescendResult<'a> {
+                                 parent_stack: Path<'a>) -> OldDescendResult<'a> {
     let order = config.new_nodes_order;
     if let Some(cand) = montecarlo::descend(order, context, cand, cut) {
-        OldDescendResult::MonteCarloLeaf(cand, pos, parent_stack)
+        OldDescendResult::MonteCarloLeaf(cand, parent_stack)
     } else { OldDescendResult::FailedMonteCarlo }
 }
 
-
-/// Called when commiting an evaluation, loop on results retrieved from ascend_node and ascend
-/// node_no_val call the corresponding function to update the tree.
-fn iter_ascend<'a>(node_arc: Arc<RwLock<Children<'a>>>,
-                   val: f64,
-                   current_pos: Option<usize>,
-                   pos_last_child: usize,
-                   parent_stack: &mut NodeStack<'a>,
-                   last_child_completed: bool) {
-    let mut is_val_inserted = true;
-    let mut node_arc = node_arc;
-    let mut completed = last_child_completed;
-    let mut pos_child = pos_last_child;
-    let mut pos_parent = current_pos;
-    loop {
-        let node_res = if is_val_inserted {
-            node_arc.write().unwrap().ascend_node(parent_stack, pos_child, completed, val)
-        } else {
-            node_arc.write().unwrap().ascend_node_no_val(parent_stack, pos_child)
-        };
-        match node_res {
-            NodeAscendResult::Children(parent_arc, pos_opt) => {
-                // If we are here, then the node we just called ascend_node on
-                // is itself a child, therefore pos_parent should be Some
-                pos_child = pos_parent.expect("parent is not a child 0");
-                pos_parent = pos_opt;
-                node_arc = parent_arc;
-            }
-            NodeAscendResult::NodeNoVal(parent_arc, pos_opt) => {
-                is_val_inserted = false;
-                pos_child = pos_parent.expect("parent is not a child 1");
-                pos_parent = pos_opt;
-                node_arc = parent_arc;
-            }
-            NodeAscendResult::NodeNotCompleted(parent_arc, pos_opt) => {
-                completed = false;
-                pos_child = pos_parent.expect("parent is not a child 2");
-                pos_parent = pos_opt;
-                node_arc = parent_arc;
-            }
-            NodeAscendResult::InvalidParent |
-            NodeAscendResult::TreeUpdated |
-            NodeAscendResult::Root => return,
-        }
-    }
-}
-
-/// We are coming from a deadend, so we did not retrieve any value here
-/// We still want to update the tree for completed children
-/// Takes a parent stack (of weak pointers) and iterates on it
-pub fn thread_ascend_tree_no_val(pos_last_child: usize, 
-                                 mut parent_stack: NodeStack) {
-    let parent_opt = parent_stack.pop();
-    if let Some((weak_node, pos_opt)) = parent_opt {
-        if let Some(node_lock) = weak_node.upgrade() {
-            iter_ascend_no_val(node_lock, pos_opt, pos_last_child, &mut parent_stack)
-        }
-    }
-}
-
-/// Called by thread_ascend_tree_no_val
-/// Iterates on the values retrieved from ascend_node_no_val
-fn iter_ascend_no_val<'a>(node_lock: Arc<RwLock<Children<'a>>>,
-                          current_pos: Option<usize>,
-                          pos_last_child: usize,
-                          parent_stack: &mut NodeStack<'a>) {
-    let mut node_arc = node_lock;
-    let mut pos_child = pos_last_child;
-    let mut pos_parent = current_pos;
-    loop {
-        let node_res;
-        {
-            node_res = node_arc.write().unwrap().ascend_node_no_val(parent_stack, pos_child);
-        }
-        match node_res {
-            NodeAscendResult::NodeNoVal(parent_arc, pos_opt) => {
-                pos_child = pos_parent.expect("IN NO VAL, parent is not a child !");
-                pos_parent = pos_opt;
-                node_arc = parent_arc;
-            }
-            NodeAscendResult::Children(..)  => { panic!("ascend_no_val returned Children"); }
-            _ => return,
-        }
-    }
-}
-
-
-
 impl<'a> Children<'a> {
-    /// pop the stack - which gives the parent of the node self, update the parent
-    /// and returns it
-    fn ascend_node(&mut self, parents_stack: &mut NodeStack<'a>, pos_child: usize,
-                   child_completed: bool,  val: f64) -> NodeAscendResult<'a> {
-        self.trim(val);
-        match self.update_as_parent(val, pos_child, child_completed) {
-            UpdateRes::KeepGoing => self.pop_parent(parents_stack, true),
-            UpdateRes::ValNotInserted =>  self.pop_parent(parents_stack, false),
-            UpdateRes::TreeUpdated => NodeAscendResult::TreeUpdated
-        }
-    }
-
-
-
-    /// Update rewards in a node with val, also replace child with NoGo if needed
-    fn update_as_parent(&mut self, val: f64, pos: usize, completed: bool) -> UpdateRes {
-        if completed {
-            self.children[pos] = SubTree::Empty;
-        }
-        let is_val_inserted = self.update_rewards(pos, val);
-        match (is_val_inserted, completed) {
-            (false, false) => UpdateRes::TreeUpdated,
-            (false, true) => UpdateRes::ValNotInserted,
-            (_, _) => UpdateRes::KeepGoing
-        }
-    }
-
-    /// Basically the same function that ascend_node, besides the fact that we know that there
-    /// is no need to update the rewards as we come from a deadend, we just need to update the
-    /// completed branches
-    fn ascend_node_no_val(&mut self, parents_stack: &mut NodeStack<'a>, pos_child: usize)
-        -> NodeAscendResult<'a> {
-        // If we call this function, we know that our child is complete (this is this function
-        // only purpose)
-        self.children[pos_child] = SubTree::Empty;
-        self.pop_parent(parents_stack, false)
-    }
-
-    /// Take a parents_stack, pop it and returns the parent node -if there is one - in a
-    /// NodeAscendResult
-    fn pop_parent(&mut self, parents_stack: &mut NodeStack<'a>, val_inserted: bool)
-        -> NodeAscendResult<'a>
-    {
-        let stack_opt = parents_stack.pop();
-        if let Some((parent_ref, pos)) = stack_opt {
-            let parent_opt = parent_ref.upgrade();
-            if let Some(ref parent_lock) = parent_opt {
-                let completed = self.children.iter().fold(true, |acc, ref x| if let
-                                                          SubTree::Empty =
-                                                          *x {acc && true} else {false});
-                match (val_inserted, completed) {
-                    (true, true) => NodeAscendResult::Children(Arc::clone(parent_lock), pos),
-                    (false, true) => NodeAscendResult::NodeNoVal(Arc::clone(parent_lock), pos),
-                    (true, false) => NodeAscendResult::NodeNotCompleted(Arc::clone(parent_lock), pos),
-                    (false, false) => NodeAscendResult::TreeUpdated
-                }
-            } else { NodeAscendResult::InvalidParent }
-        } else { NodeAscendResult::Root }
-    }
-
-
-    /// Update a rewards list given a new value and the position where it was found
-    /// returns true if the value was inserted in the node
-    fn update_rewards(&mut self, pos: usize, val: f64) -> bool {
-        let total_trials = self.rewards.iter().map(|x| x.0.len()).sum::<usize>();
-        // If total trials is less than THRESHOLD, then we simply push our new value
-        // in the node where it was found.
-        if total_trials < 10 { // FIXME: use the real threshold
-            self.rewards[pos].0.push(val);
-            true
-        } else {
-            // Now we have to find the minimum value of all vectors and the place where
-            // we found it.
-            let (ind, int_ind, min) = unwrap!(self.find_max_rewards());
-            if val < min {
-                self.rewards[ind].0.swap_remove(int_ind);
-                self.rewards[pos].0.push(val);
-                true
-            } else { false }
-        }
-    }
-
-    /// Returns the tuple (outer_index, int_index, max) which is the maximum value found
-    /// in rewards with the indexes where we found it - index on outer dimension, then on
-    /// inner.
-    fn find_max_rewards(&self) -> Option<(usize, usize, f64)> {
-        self.rewards.iter().enumerate().flat_map(|(out_idx, rewards)| {
-            let max = rewards.0.iter().cloned().enumerate()
-                .max_by(|lhs, rhs| cmp_f64(lhs.1, rhs.1));
-            max.map(|(idx, value)| (out_idx, idx, value))
-        }).max_by(|x1, x2| cmp_f64(x1.2, x2.2))
-    }
-
     /// We have a newly expanded node, we want to do a montecarlo descend on it
     /// As we cannot directly own the original candidate (which must stay in the tree, and
     /// which we do not want to clone) we have to do this on the freshly expanded node.
