606d5c762e07f8473714ab6fc241c448db18aaa6
Nicolas Tollenaere <nicolas.tollenaere@inria.com>
2018-04-24 11:44:20 -0400

    Generated code compiles, not linking

---
+++ src/device/x86/compile.rs
@@ -0,0 +1,35 @@
+use libc;
+use std::process::{ExitStatus, Command};
+use std::fs::File;
+use std::io::{Seek, SeekFrom};
+use std::time::Instant;
+use std;
+use libloading;
+
+pub fn compile(mut source_file: File, lib_path: &String) -> ExitStatus {
+    unwrap!(source_file.seek(SeekFrom::Start(0)));
+    Command::new("gcc")
+        .stdin(source_file)
+        .arg("-shared")
+        .arg("-fPIC")
+        .arg("-o")
+        .arg(lib_path)
+        .arg("-xc")
+        .arg("-")
+        .status()
+        .expect("Could not execute gcc")
+}
+
+pub fn link_and_exec(lib_path: &String, fun_name: &String, mut args: Vec<*mut libc::c_void> ) -> f64 {
+    let lib = libloading::Library::new(lib_path)
+        .expect("Library not found");
+    unsafe {
+        let func : libloading::Symbol<unsafe extern fn(*mut *mut libc::c_void) > = 
+            lib.get(fun_name.as_bytes())
+            .expect("Could not find symbol in library");
+        let t0 = Instant::now();
+        func(args.as_mut_ptr());
+        let t = Instant::now() - t0;
+        t.subsec_nanos() as f64
+    }
+}
