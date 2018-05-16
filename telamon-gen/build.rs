2bb33372770f78f9d7679096c412d5cbcd24b0de
adjivas <adjivas@users.noreply.github.com>
2018-05-16 18:21:30 +0200

    add poc.l to dependency condition of Cargo rebuilt

--- telamon-gen/build.rs
+++ telamon-gen/build.rs
@@ -6,11 +6,13 @@ extern crate lalrpop;
 fn add_dependency(dep: &str) { println!("cargo:rerun-if-changed={}", dep); }
 
 fn main() {
-    // Compile the lexer.(`LEX="flex" cargo build --features "lex"`)
+    // Regenerate the lexer.(`LEX="flex" cargo build --features "lex"`)
     #[cfg(feature = "lex")]
     {
         use std::{env,process::Command};
 
+        // Generate the lexer .             
+        add_dependency("src/poc.l");
         let bin = env::var("LEX").unwrap_or(String::from("flex"));
 
         Command::new(bin)
@@ -20,6 +22,7 @@ fn main() {
                 .expect("failed to execute Flex's process");
     }
 
+    // Compile the lexer .             
     cc::Build::new()
             .file("exh.c")
             .flag("-Wno-unused-parameter")
