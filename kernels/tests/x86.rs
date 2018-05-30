1bc4d8a2d0f9e85a0b55599678dcea2456bc0c90
Ulysse Beaugnon <ulysse.beaugnon@ens.fr>
2018-05-30 00:30:02 -0400

    fix tests

--- kernels/tests/x86.rs
+++ kernels/tests/x86.rs
@@ -16,7 +16,7 @@ macro_rules! test_output {
     }
 }
 
-test_output!(axpy, linalg::Axpy<f32>, 100, (1 << 16, bool));
-test_output!(mv, linalg::MatVec<f32>, 100, (1<<4, 1<<2, bool));
-test_output!(gesummv, linalg::Gesummv<f32>, 100, (1<<4, 1<<4, bool));
+test_output!(axpy, linalg::Axpy<f32>, 100, (1 << 16, true));
+test_output!(mv, linalg::MatVec<f32>, 100, (1<<4, 1<<2, true));
+test_output!(gesummv, linalg::Gesummv<f32>, 100, (1<<4, 1<<4, true));
 test_output!(matmul, linalg::MatMul<f32>, 100, linalg::MatMulP::new(16, 16, 16));
