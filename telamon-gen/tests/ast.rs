6b017163fe988beffd3c76e4652c53b211b7c933
adjivas <adjivas@users.noreply.github.com>
2018-06-05 17:58:17 +0200

    test enum_multi_name_field

--- telamon-gen/tests/ast.rs
+++ telamon-gen/tests/ast.rs
@@ -1,20 +1,13 @@
 extern crate telamon_gen;
 extern crate lalrpop_util;
 
-use telamon_gen::lexer::{Lexer, Token, LexicalError, Position};
-use telamon_gen::lexer::*;
+use telamon_gen::lexer::{Lexer, Spanned, Position};
+use telamon_gen::ast::EnumStatement;
 use telamon_gen::parser;
-use telamon_gen::error;
-use telamon_gen::ast;
-
-use lalrpop_util::ParseError;
-
-use std::path::Path;
 
 #[test]
-fn duplicata_enum() {
-    let ast: ast::Ast =
-        parser::parse_ast(Lexer::from(
+fn enum_name_multi() {
+    assert_eq!(parser::parse_ast(Lexer::from(
         b"define enum foo():
           value A:
           value B:
@@ -23,9 +16,29 @@ fn duplicata_enum() {
           
           define enum foo():
           value A:
-          end".to_vec())).unwrap();
+          end".to_vec())).unwrap().type_check().err(),
+        Some(Spanned {
+            leg: Position { line: 6, column: 10},
+            end: Position { line: 6, column: 28},
+            data: telamon_gen::ast::TypeError::EnumNameMulti
+        })
+    );
+}
 
-    println!("{:?}", ast);
-    
-    println!("{:?}", ast.type_check().err());
+#[test]
+fn enum_multi_name_field() {
+    assert_eq!(parser::parse_ast(Lexer::from(
+        b"define enum foo():
+          value A:
+          value B:
+          value A:
+          end".to_vec())).unwrap().type_check().err(),
+        Some(Spanned {
+            leg: Position { line: 0, column: 0},
+            end: Position { line: 0, column: 18},
+            data: telamon_gen::ast::TypeError::EnumMultipleNameField(
+                EnumStatement::Value(String::from("A"), None, vec![])
+            ),
+        })
+    );
 }
