93821d45081ffe2ddbbe47e010083cf664db35a0
adjivas <adjivas@users.noreply.github.com>
2018-06-14 19:10:42 +0200

    add undefined parametric check

--- telamon-gen/tests/typing/integer.rs
+++ telamon-gen/tests/typing/integer.rs
@@ -7,13 +7,22 @@ pub use super::telamon_gen::ast::*;
 #[test]
 fn integer_redefinition() {
     assert_eq!(parser::parse_ast(Lexer::from(
-        b"define integer foo($myarg in MySet) in \"mycode\"
+        b"set MySet:
+            item_type = \"ir::inst::Obj\"
+            id_type = \"ir::inst::Id\"
+            item_getter = \"ir::inst::get($fun, $id)\"
+            id_getter = \"ir::inst::Obj::id($item)\"
+            iterator = \"ir::inst::iter($fun)\"
+            var_prefix = \"inst\"
+            new_objs = \"$objs.inst\"
+          end
+          define integer foo($myarg in MySet) in \"mycode\"
           end
           define integer foo($myarg in MySet) in \"mycode\"
           end".to_vec())).unwrap().type_check().err(),
         Some(Spanned {
-            beg: Position { line: 2, column: 10},
-            end: Position { line: 3, column: 13},
+            beg: Position { line: 11, column: 10},
+            end: Position { line: 12, column: 13},
             data: TypeError::Redefinition(
                 String::from("foo"),
                 Hint::Integer
@@ -21,7 +30,16 @@ fn integer_redefinition() {
         })
     );
     assert!(parser::parse_ast(Lexer::from(
-        b"define integer foo($myarg in MySet) in \"mycode\"
+        b"set MySet:
+            item_type = \"ir::inst::Obj\"
+            id_type = \"ir::inst::Id\"
+            item_getter = \"ir::inst::get($fun, $id)\"
+            id_getter = \"ir::inst::Obj::id($item)\"
+            iterator = \"ir::inst::iter($fun)\"
+            var_prefix = \"inst\"
+            new_objs = \"$objs.inst\"
+          end
+          define integer foo($myarg in MySet) in \"mycode\"
           end".to_vec())).unwrap().type_check().is_ok()
     );
 
@@ -30,15 +48,37 @@ fn integer_redefinition() {
 #[test]
 fn integer_field_redefinition() {
     assert_eq!(parser::parse_ast(Lexer::from(
-        b"define integer foo($arg in MySet, $arg in MySet) in \"mycode\"
+        b"set MySet:
+            item_type = \"ir::inst::Obj\"
+            id_type = \"ir::inst::Id\"
+            item_getter = \"ir::inst::get($fun, $id)\"
+            id_getter = \"ir::inst::Obj::id($item)\"
+            iterator = \"ir::inst::iter($fun)\"
+            var_prefix = \"inst\"
+            new_objs = \"$objs.inst\"
+          end
+          define integer foo($arg in MySet, $arg in MySet) in \"mycode\"
           end".to_vec())).unwrap().type_check().err(),
         Some(Spanned {
-            beg: Position { line: 0, column: 0},
-            end: Position { line: 1, column: 13},
+            beg: Position { line: 9, column: 10},
+            end: Position { line: 10, column: 13},
             data: TypeError::Redefinition(
                 String::from("arg"),
                 Hint::IntegerAttribute,
             )
+        })
+    );
+}
+
+#[test]
+fn integer_undefined_parametric() {
+    assert_eq!(parser::parse_ast(Lexer::from(
+        b"define integer foo($arg in MySet) in \"mycode\"
+          end".to_vec())).unwrap().type_check().err(),
+        Some(Spanned {
+            beg: Position { line: 0, column: 0},
+            end: Position { line: 1, column: 13},
+            data: TypeError::Undefined(String::from("MySet")),
         })
     );
 }
