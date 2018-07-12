018f3354f15324d0ba0a60ca5e6f953df5a0e89e
Basile Clement <Elarnon@users.noreply.github.com>
2018-07-12 15:02:51 +0000

    Introduce bare-bones C and Python API (#53)
    * Introduce bare-bones C and Python API
    
    This adds a new telamon-capi crate exporting a C API through a dynamic
    library, and a Python package which uses said C API crate to expose a
    Python API for Telamon. For the moment the API is very simple and only
    allows optimising a MatMul kernel on either an x86 or cuda device, but
    should evolve over time to incorporate additional features such as
    driving the exploration from Python.
    
    Fixes #49
    
    * Address review comments
    
     - Add documentation to the C API lib
     - Remove generated C header from source control

--- src/device/x86/context.rs
+++ src/device/x86/context.rs
@@ -148,7 +148,6 @@ enum ThunkArg {
 /// returns the time elapsed. Converts ThunkArgs to HoldTHunk as we want to allocate memory for
 /// temporary arrays at the last possible moment
 fn function_evaluate(fun_str: &String, args: &Vec<ThunkArg>) -> Result<f64, ()> {
-    println!("{}", fun_str);
     let temp_dir = unwrap!(tempfile::tempdir());
     let templib_name = temp_dir.path().join("lib_compute.so").to_string_lossy().into_owned();
     let mut source_file = unwrap!(tempfile::tempfile());
