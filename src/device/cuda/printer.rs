ff0799f592adc9dd4ed1682134d23a41ada024d4
Ulysse Beaugnon <ulysse.beaugnon@ens.fr>
2018-07-13 12:46:18 -0400

    format part of the printer

--- src/device/cuda/printer.rs
+++ src/device/cuda/printer.rs
@@ -66,8 +66,13 @@ impl CudaPrinter {
 
     fn get_inst_type(mode: MulMode, ret_type: Type) -> Type {
         match mode {
-            MulMode::Wide => if let Type::I(n) = ret_type {Type::I( n / 2)} else {panic!("get_inst_type should only be called with integer types")}
-            ,
+            MulMode::Wide => {
+                if let Type::I(n) = ret_type {
+                    Type::I( n / 2)
+                } else {
+                    panic!("get_inst_type should only be called with integer types")
+                }
+            },
             _ => ret_type,
         }
     }
@@ -233,7 +238,8 @@ impl CudaPrinter {
                         let name = name_map.declare_size_cast(incr, level.t());
                         if let Some(name) = name {
                             let old_name = name_map.name_size(incr, Type::I(32));
-                            self.print_cast(Type::I(32), level.t(), op::Rounding::Exact, &name, &old_name);
+                            self.print_cast(Type::I(32), level.t(), op::Rounding::Exact,
+                                            &name, &old_name);
                         }
                     }
                 }
@@ -334,61 +340,92 @@ impl Printer for CudaPrinter {
  }
 
     /// Print return_id = op1 op op2
-    fn print_binop(&mut self, op: ir::BinOp, return_type: Type, rounding: op::Rounding, return_id: &str, lhs: &str, rhs: &str) { 
-    unwrap!(writeln!(self.out_function, "{}{}.{} {}, {}, {};", Self::binary_op(op), Self::rounding(rounding), Self::get_type(return_type), return_id, lhs, rhs));
+    fn print_binop(&mut self, op: ir::BinOp,
+                   return_type: Type,
+                   rounding: op::Rounding,
+                   return_id: &str, lhs: &str, rhs: &str) { 
+        let op = Self::binary_op(op);
+        let rounding = Self::rounding(rounding);
+        let ret_type = Self::get_type(return_type);
+        unwrap!(writeln!(self.out_function, "{}{}.{} {}, {}, {};",
+                         op, rounding, ret_type, return_id, lhs, rhs));
     }
 
     /// Print return_id = op1 * op2
-    fn print_mul(&mut self, return_type: Type, round: op::Rounding, mul_mode: MulMode, return_id: &str, lhs: &str, rhs: &str) {
+    fn print_mul(&mut self, return_type: Type,
+                 round: op::Rounding,
+                 mul_mode: MulMode,
+                 return_id: &str,
+                 lhs: &str, rhs: &str) {
         let operator = if round == op::Rounding::Exact {
             format!("mul{}", Self::mul_mode(mul_mode))
         } else {
             format!("mul{}", Self::rounding(round))
         };
-        unwrap!(writeln!(self.out_function, "{}.{} {}, {}, {};", operator, Self::get_type(Self::get_inst_type(mul_mode, return_type)), return_id, lhs, rhs));
+        let t = Self::get_type(Self::get_inst_type(mul_mode, return_type));
+        unwrap!(writeln!(self.out_function, "{}.{} {}, {}, {};",
+                         operator, t, return_id, lhs, rhs));
     }
 
     /// Print return_id = mlhs * mrhs + arhs
-    fn print_mad(&mut self, ret_type: Type, round: op::Rounding, mul_mode: MulMode, return_id: &str,  mlhs: &str, mrhs: &str, arhs: &str) {
+    fn print_mad(&mut self, ret_type: Type,
+                 round: op::Rounding,
+                 mul_mode: MulMode,
+                 return_id: &str,
+                 mlhs: &str, mrhs: &str, arhs: &str) {
         let operator = if round == op::Rounding::Exact {
             format!("mad{}", Self::mul_mode(mul_mode))
         } else {
             format!("fma{}", Self::rounding(round))
         };
-        unwrap!(writeln!(self.out_function, "{}.{} {}, {}, {}, {};", operator, Self::get_type(Self::get_inst_type(mul_mode, ret_type)), return_id, mlhs, mrhs, arhs));
+        let t = Self::get_type(Self::get_inst_type(mul_mode, return_type));
+        unwrap!(writeln!(self.out_function, "{}.{} {}, {}, {}, {};",
+                         operator, t, return_id, mlhs, mrhs, arhs));
     }
 
     /// Print return_id = op 
     fn print_mov(&mut self, return_type: Type, return_id: &str, op: &str) {
-        unwrap!(writeln!(self.out_function, "mov.{} {}, {};", Self::get_type(return_type), return_id, op));
+        unwrap!(writeln!(self.out_function, "mov.{} {}, {};",
+                         Self::get_type(return_type), return_id, op));
     }
 
     /// Print return_id = load [addr] 
     fn print_ld(&mut self, return_type: Type, flag: InstFlag, return_id: &str,  addr: &str) {
-        unwrap!(writeln!(self.out_function, "{}.{} {}, [{}];", Self::ld_operator(flag), Self::get_type(return_type), return_id,  addr));
+        let operator = Self::ld_operator(flag);
+        unwrap!(writeln!(self.out_function, "{}.{} {}, [{}];",
+                         operator, Self::get_type(return_type), return_id,  addr));
     }
 
     /// Print store val [addr] 
     fn print_st(&mut self, val_type: Type, mem_flag: InstFlag, addr: &str, val: &str) {
         let operator = Self::st_operator(mem_flag);
-        unwrap!(writeln!(self.out_function, "{}.{} [{}], {};", operator, Self::get_type(val_type), addr, val));
+        unwrap!(writeln!(self.out_function, "{}.{} [{}], {};",
+                         operator, Self::get_type(val_type), addr, val));
     }
 
     /// Print if (cond) store val [addr] 
-    fn print_cond_st(&mut self, val_type: Type, mem_flag: InstFlag, cond: &str, addr: &str, val: &str) {
+    fn print_cond_st(&mut self, val_type: Type,
+                     mem_flag: InstFlag,
+                     cond: &str, addr: &str, val: &str) {
         let operator = Self::st_operator(mem_flag);
-        unwrap!(writeln!(self.out_function, "@{} {}.{} [{}], {};", cond, operator, Self::get_type(val_type), addr, val));
+        unwrap!(writeln!(self.out_function, "@{} {}.{} [{}], {};",
+                         cond, operator, Self::get_type(val_type), addr, val));
     }
 
     /// Print return_id = (val_type) val
-    fn print_cast(&mut self, from_t: Type, to_t: Type, round: op::Rounding, return_id: &str, op1: &str) {
-        let operator = format!("cvt{}.{}.{}", Self::rounding(round), Self::get_type(to_t), Self::get_type(from_t));
+    fn print_cast(&mut self, from_t: Type, to_t: Type,
+                  round: op::Rounding,
+                  return_id: &str,
+                  op1: &str) {
+        let rounding = Self::rounding(round);
+        let to_t = Self::get_type(to_t);
+        let from_t = Self::get_type(from_t);
+        let operator = format!("cvt{}.{}.{}", rounding, to_t, from_t);
         unwrap!(writeln!(self.out_function, "{} {}, {};",  operator, return_id, op1));
     }
 
     /// print a label where to jump
     fn print_label(&mut self, label_id: &str) {
-        //self.out_function.push_str(&format!("LOOP_{}:", label_id));
         unwrap!(writeln!(self.out_function, "LOOP_{}:", label_id));
     }
 
@@ -427,7 +464,10 @@ impl Printer for CudaPrinter {
         unwrap!(writeln!(self.out_function, "bar.sync 0;"));
     }
 
-    fn print_vector_inst(&mut self, inst: &Instruction, dim: &Dimension, namer: &mut NameMap, fun: &Function) {
+    fn print_vector_inst(&mut self, inst: &Instruction,
+                         dim: &Dimension,
+                         namer: &mut NameMap,
+                         fun: &Function) {
         let size = unwrap!(dim.size().as_int());
         let flag = unwrap!(inst.mem_flag());
         match *inst.operator() {
