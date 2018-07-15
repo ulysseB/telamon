da6463f94d9f609eb0401a71b0d22dc3544d82af
Ulysse Beaugnon <ulysse.beaugnon@ens.fr>
2018-07-15 14:57:06 +0200

    revamp locking. Only workins within a process

--- src/bin/cuda_characterize.rs
+++ src/bin/cuda_characterize.rs
@@ -1,62 +1,16 @@
 extern crate env_logger;
-extern crate itertools;
 extern crate telamon;
-extern crate getopts;
+extern crate serde_json;
+#[macro_use]
+extern crate telamon_utils;
 
-use itertools::Itertools;
-use getopts::Options;
 use telamon::device::cuda;
-
-#[derive(Default)]
-struct Config {
-    write_to_file: bool,
-}
-
-impl Config {
-    fn parse() -> Self {
-        let mut config = Self::default();
-        let mut opts = Options::new();
-        opts.optflag("h", "help", "Print the help menu.");
-        opts.optflag("w", "write", "Write the gpu description to the description file.");
-        let args = std::env::args().collect_vec();
-        let opt_matches = match opts.parse(&args[1..]) {
-            Ok(x) => x,
-            Err(x) => {
-                println!("{} Use '--help' to display a list of valid options.", x);
-                std::process::exit(-1)
-            },
-        };
-        if opt_matches.opt_present("h") {
-            let brief = opts.short_usage(&args[0]);
-            println!("{}", opts.usage(&brief));
-            std::process::exit(0);
-        }
-        config.write_to_file = opt_matches.opt_present("w");
-        config
-    }
-}
 
 fn main() {
     env_logger::init();
-    let config = Config::parse();
     let executor = cuda::Executor::init();
     let gpu = cuda::characterize::characterize(&executor);
-
-    if config.write_to_file {
-        unimplemented!(); // FIXME
-        /*let mut gpu_list = match gpu_list() {
-            Ok(x) => x.into_iter().filter(|x| x.name != gpu.name).collect_vec(),
-            Err(s) => {
-                println!("Unable to load the existing GPU descriptions: {}", s);
-            },
-            };
-            gpu_list.push(gpu);
-            let mut file = std::fs::File::create("data/cuda_gpus.json").unwrap();
-            write!(file, "{}", json::as_pretty_json(&gpu_list)).unwrap();
-        } else {
-            println!("{}", json::as_pretty_json(&gpu));
-        }*/
-    }
+    unwrap!(serde_json::to_writer_pretty(std::io::stdout(), &gpu));
     //instruction::print_smx_bandwidth(&gpu, &executor);
     //instruction::print_smx_store_bandwidth(&gpu, &executor);*/
 }
--- src/device/cuda/characterize/mod.rs
+++ src/device/cuda/characterize/mod.rs
@@ -9,10 +9,8 @@ use self::table::Table;
 
 use device::cuda;
 use itertools::Itertools;
-use fs2::FileExt;
 use serde_json;
 use std;
-use std::io::Seek;
 use xdg;
 
 /// Error raised will retrieveing the GPU description
@@ -27,29 +25,24 @@ enum Error {
 /// Retrieve the description of the GPU from the description file. Updates it if needed.
 pub fn get_gpu_desc(executor: &cuda::Executor) -> cuda::Gpu {
     let config_path = get_config_path();
-    let mut file = unwrap!(std::fs::OpenOptions::new()
-        .read(true).write(true).create(true)
-        .open(config_path));
-    // FIXME: is file locking working as expected
-    unwrap!(file.try_lock_exclusive().or_else(|err| match err.kind() {
-        std::io::ErrorKind::WouldBlock => {
-            warn!("GPU description file is already in use, waiting");
-            file.lock_exclusive()
-        },
-        _ => Err(err)
-    }));
-    unwrap!(file.seek(std::io::SeekFrom::Start(0)));
-    let gpu = serde_json::from_reader(&file).map_err(Error::Parser)
+    lazy_static! {
+        // Ensure that at most one thread runs the characterization.
+        static ref LOCK: std::sync::Mutex<()> = Default::default();
+    }
+    let lock = unwrap!(LOCK.lock());
+    let gpu = serde_json::from_reader(&unwrap!(std::fs::File::open(&config_path)))
+        .map_err(Error::Parser)
         .and_then(|gpu: cuda::Gpu| {
             let name = executor.device_name();
             if gpu.name == name { Ok(gpu) } else { Err(Error::WrongGpu(name)) }
         }).unwrap_or_else(|err| {
             warn!("{}. Runing characterization.", err);
             let gpu = characterize(executor);
-            // FIXME: print the GPU
+            let out = unwrap!(std::fs::File::create(&config_path));
+            unwrap!(serde_json::to_writer_pretty(out, &gpu));
             gpu
         });
-    unwrap!(file.unlock());
+    std::mem::drop(lock);
     gpu
 }
 
