5b8c9fac8b1641e07f53126b1554eedf8ff8f149
Ulysse Beaugnon <ulysse.beaugnon@ens.fr>
2018-07-16 22:48:38 +0200

    create a result_value field in instruction

--- src/ir/instruction.rs
+++ src/ir/instruction.rs
@@ -1,6 +1,7 @@
 //! Describes the instructions.
 use ir::{self, BBId, DimMapScope, LoweringMap, Operand, Operator, Statement, Type};
 use std;
+use std::hash::{Hash, Hasher};
 use utils::*;
 
 /// Uniquely identifies an instruction.
@@ -28,6 +29,7 @@ pub struct Instruction<'a, L = LoweringMap> {
     operator: Operator<'a, L>,
     id: InstId,
     iter_dims: HashSet<ir::DimId>,
+    value: Option<ir::ValueId>,
 }
 
 impl<'a, L> Instruction<'a, L> {
@@ -140,6 +142,15 @@ impl<'a, L> Instruction<'a, L> {
     /// iteration dimension.
     pub fn add_iteration_dimension(&mut self, dim: ir::DimId) -> bool {
         self.iter_dims.insert(dim)
+    }
+
+    /// Returns the `Value` holding the result of this instruction.
+    pub fn result_value(&self) -> Option<ir::ValueId> { self.value }
+
+    /// Sets the `Value` holdings the result of this instruction.
+    pub fn set_result_value(&mut self, value: ir::ValueId) {
+        // An instruction value cannot be set twice.
+        assert_eq!(std::mem::replace(&mut self.value, Some(value)), None);
     }
 }
 
