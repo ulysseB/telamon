632250fbee3cf92aa33ae361eb54ae7677ce4fa8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-16 12:31:22 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["telamon"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"telamon/model/struct.HwPressure.html\" title=\"struct telamon::model::HwPressure\">HwPressure</a>&gt; for <a class=\"struct\" href=\"telamon/device/cuda/struct.InstDesc.html\" title=\"struct telamon::device::cuda::InstDesc\">InstDesc</a>",synthetic:false,types:["telamon::device::cuda::gpu::InstDesc"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
