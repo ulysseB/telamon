c290495d55835ca0b243f9ecc335ed0fdac460de
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-17 18:37:58 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["telamon"] = [{text:"impl Fail for <a class=\"enum\" href=\"telamon/ir/enum.TypeError.html\" title=\"enum telamon::ir::TypeError\">TypeError</a>",synthetic:false,types:["telamon::ir::error::TypeError"]},{text:"impl Fail for <a class=\"enum\" href=\"telamon/ir/enum.Error.html\" title=\"enum telamon::ir::Error\">Error</a>",synthetic:false,types:["telamon::ir::error::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
