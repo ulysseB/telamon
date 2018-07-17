d3aadd2e25accae6b3f81f9c09c4b86842143070
Basile Clement <basile@clement.pm>
2018-07-17 09:34:14 +0200

    Use vec! instead of Vec::new

--- kernels/src/linalg.rs
+++ kernels/src/linalg.rs
@@ -337,13 +337,13 @@ impl<'a, S: Scalar> Kernel<'a> for MatMul<'a, S> {
     fn build_body<'b>(&self, signature: &'b ir::Signature, ctx: &'b device::Context)
         -> Vec<Candidate<'b>>
     {
-        let m_tiles = self.params.m_tiling.clone().map(Vec::new).unwrap_or_else(|| {
+        let m_tiles = self.params.m_tiling.clone().map(|x| vec![x]).unwrap_or_else(|| {
             ::generate_tile_sizes(self.params.m as u32, &[64, 8])
         });
-        let n_tiles = self.params.n_tiling.clone().map(Vec::new).unwrap_or_else(|| {
+        let n_tiles = self.params.n_tiling.clone().map(|x| vec![x]).unwrap_or_else(|| {
             ::generate_tile_sizes(self.params.n as u32, &[64, 8])
         });
-        let k_tiles = self.params.k_tiling.clone().map(Vec::new).unwrap_or_else(|| {
+        let k_tiles = self.params.k_tiling.clone().map(|x| vec![x]).unwrap_or_else(|| {
             ::generate_tile_sizes(self.params.k as u32, &[64])
         });
         let tilings = ::par_iter_product(::par_iter_product(m_tiles, n_tiles), k_tiles);
