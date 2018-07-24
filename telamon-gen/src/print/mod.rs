dcf21543e9f85e6bc73ad972801336320b49ea40
Basile Clement <basile@clement.pm>
2018-07-24 18:06:48 +0200

    Ensure telamon-gen output is deterministic
    We want telamon-gen output to be deterministic on a given .exh file
    because we want to be able to assume field and enum order when
    serializing to compact binary formats. Currently telamon-gen is
    deterministic (thanks to using the non-random HashMaps from
    telamon-utils) up to the printer, where we use a topological sort from
    the `pathfinding` crate to ensure counters are serialized after their
    dependencies. Unfortunately, the topological sort in the `pathfinding`
    crate is non-deterministic due to its use of regular Rust HashMaps.
    
    This patch replaces the topological sort from the `pathfinding` crate
    with our own topological sort implementation,
    `stable_topological_sort`, which we guarantee is not only
    non-deterministic but is also stable: the order between elements which
    don't have (transitive) dependencies to each other is preserved.
    
    In addition, it replaces the use of some HashMaps with IndexMaps from
    the `indexmap` crate, which are hash tables ensuring that iteration
    order is identical to insertion order until any value is removed. This
    allows to help propagate the stability guarantee to the order in which
    choices are defined in the `.exh` file, giving some control on the
    generated code to the user.

--- telamon-gen/src/print/mod.rs
+++ telamon-gen/src/print/mod.rs
@@ -3,9 +3,13 @@ use handlebars::{self, Handlebars, Helper, Renderable, RenderContext, RenderErro
 use ir;
 use itertools::Itertools;
 use serde_json::value::Value as JsonValue;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 use std::fmt::{self, Display, Formatter};
+use std::hash::Hash;
+use std::iter::FromIterator;
 //use std::io::prelude::*;
-use pathfinding::prelude::topological_sort;
+use indexmap::IndexMap;
 use utils::*;
 
 // TODO(cleanup): use handlebars instead of printer macros and static templates
@@ -201,6 +205,8 @@ mod store;
 mod value_set;
 
 use self::store::PartialIterator;
+#[cfg(test)]
+pub(crate) use self::ast::Variable;
 
 /// Generate the trigger code to add a representant to a quotient set.
 pub fn add_to_quotient(set: &ir::SetDef,
@@ -240,10 +246,265 @@ pub fn print(ir_desc: &ir::IrDesc) -> String {
     )
 }
 
+/// Find a topological order in a directed graph.
+///
+/// The topological order is guaranteed to be stable, i.e. the
+/// relative position of elements which are not (transitively) ordered
+/// is preserved.
+///
+/// # Return value
+///
+/// This function will either return `Ok` with a stable topological
+/// order or, if there are cycles in the ordering defined by
+/// `predecessors`, `Err` with a pair `(sorted, cycles)` where
+/// `cycles` contains all the nodes which are part of a cycle, and
+/// `sorted` contains a stable topological order of the remaining
+/// nodes.
+///
+/// # Arguments
+///
+/// * `nodes` - A slice that holds the nodes to sort. Must not contain
+///   duplicates.
+/// * `predecessors` - A function returning an iterable of
+///   predecessors or dependencies for each node. Will be called
+///   exactly once for each node in `nodes`. Each element of the
+///   returned iterator must be in `nodes.
+///
+/// # Panics
+///
+/// This function will panic if there are duplicates in `nodes` or if
+/// `predecessors` return a value which is not in `nodes`.
+///
+/// # Examples
+///
+/// See the `stable_topological_sort_tests` module.
+fn stable_topological_sort<'a, N, P, I>(
+    nodes: &[N],
+    predecessors: P,
+) -> Result<Vec<N>, (Vec<N>, Vec<N>)>
+where
+    N: Eq + Hash + Clone,
+    P: Fn(&N) -> I,
+    I: IntoIterator<Item = N>,
+{
+    /// Data about a node used by the topological sort algorithm.
+    #[derive(Eq)]
+    struct NodeData {
+        /// The index of the node in `nodes`
+        index: usize,
+        /// The number of predecessors. Nodes that have already been
+        /// sorted are no longer considered as predecessors.
+        num_predecessors: usize,
+        /// The indices in `nodes` of this node's successors.
+        successors: Vec<usize>,
+    }
+
+    /// `PartialEq` is implemented manually since it needs to be
+    /// compatible with `Ord` and `PartialOrd`. Note that technically
+    /// we will only ever create a single `NodeData` with a given
+    /// `index` value, so the default implementation would only be
+    /// wrong conceptually (although slower than the manual version).
+    impl PartialEq for NodeData {
+        fn eq(&self, other: &NodeData) -> bool {
+            self.index == other.index
+        }
+    }
+
+    /// Custom `Ord` implementation to only take the `index` into
+    /// account. Note that we want the `BinaryHeap` to act as a
+    /// min-heap below, so we reverse the ordering here.
+    impl Ord for NodeData {
+        fn cmp(&self, other: &NodeData) -> Ordering {
+            self.index.cmp(&other.index).reverse()
+        }
+    }
+
+    impl PartialOrd for NodeData {
+        fn partial_cmp(&self, other: &NodeData) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl NodeData {
+        fn new(index: usize) -> NodeData {
+            NodeData {
+                index,
+                num_predecessors: 0,
+                successors: Vec::new(),
+            }
+        }
+    }
+
+    // We need an index map to map back from the predecessors into their indices.
+    let mut node_data: Vec<_> = (0..nodes.len()).map(NodeData::new).map(Some).collect();
+    let index_map: HashMap<_, _> =
+        HashMap::from_iter(nodes.iter().enumerate().map(|(index, node)| (node, index)));
+    if index_map.len() != nodes.len() {
+        panic!("duplicate nodes found");
+    }
+
+    // Properly set the predecessors and successors for each node.
+    for (index, node) in nodes.iter().enumerate() {
+        let num_predecessors = predecessors(node)
+            .into_iter()
+            .map(|pred| {
+                let &pred_index =
+                    index_map.get(&pred).expect("predecessor is not in graph");
+                node_data[pred_index]
+                    .as_mut()
+                    .unwrap()
+                    .successors
+                    .push(index)
+            })
+            .count();
+
+        node_data[index].as_mut().unwrap().num_predecessors = num_predecessors
+    }
+
+    // Build the priority queue containing the nodes which have no
+    // predecessors. Note that we must loop again to create this
+    // instead of `take`ing directly from `node_data` when counting
+    // the predecessors because otherwise we would throw off the
+    // predecessor count for the succesors, and decrementing that
+    // count for the nodes we have already moved to the queue would
+    // violate the stability guarantee.
+    let mut queue = BinaryHeap::with_capacity(nodes.len());
+    for data in node_data.iter_mut() {
+        if data.as_mut().unwrap().num_predecessors == 0 {
+            queue.push(data.take().unwrap())
+        }
+    }
+
+    // At each iteration of this loop we maintain the invariant that
+    // the priority queue contains exactly the nodes for which all
+    // predecessors are in sorted, but have not been sorted
+    // themselves. Using a priority queue ensures that we are always
+    // processing nodes in the order they appear in the initial
+    // `nodes` slice.
+    //
+    // Whenever we move a node from the queue to the `sorted` array,
+    // we update all its successors (if any) by decrementing its
+    // number of predecessors. Whenever the number of predecessors
+    // reaches `0`, this node was the last predecessor not already
+    // sorted, and we add the successor to the priority queue.
+    let mut sorted = Vec::with_capacity(nodes.len());
+    while let Some(data) = queue.pop() {
+        sorted.push(nodes[data.index].clone());
+
+        for &successor_index in data.successors.iter() {
+            let mut successor_data = &mut node_data[successor_index];
+            successor_data.as_mut().unwrap().num_predecessors -= 1;
+            if successor_data.as_mut().unwrap().num_predecessors == 0 {
+                queue.push(successor_data.take().unwrap())
+            }
+        }
+    }
+
+    // If there is a cycle, we will never put it in the queue because
+    // there always will be another element of the cycle as a
+    // successor, and so `sorted` will be smaller than `nodes.We can
+    // get back all the elements which belong to a cycle by looking
+    // for the non-empty indices in `node_data`.
+    if sorted.len() != nodes.len() {
+        let mut cycles = Vec::with_capacity(nodes.len() - sorted.len());
+        for (index, data) in node_data.into_iter().enumerate() {
+            if data.is_some() {
+                cycles.push(nodes[index].clone())
+            }
+        }
+        Err((sorted, cycles))
+    } else {
+        Ok(sorted)
+    }
+}
+
+/// Note that the code in the `stable_topological_sort_tests` omdule
+/// should be in an Examples sections in the `stable_topological_sort`
+/// function, but we can't run rustdoc tests on private functions for
+/// some reason.
+///
+/// For the sake of concision and readability, the examples use
+/// letters to represent nodes and `:` to separate a node from its
+/// dependencies. For instance, `"d"` is a node called `"d"` without
+/// dependencies, `"e:d"` is a node called `"e"` with a single
+/// dependency on the node `"d"`, and `"f:e,q"` is a node called `"f"`
+/// with two dependencies on nodes `"e"` and `"q"`.
+#[cfg(test)]
+mod stable_topological_sort_tests {
+    use super::stable_topological_sort;
+
+    /// Sorting an array with no dependencies does not modify the
+    /// order.
+    #[test]
+    fn stable_no_deps() {
+        assert_eq!(
+            stable_topological_sort(&vec!["a", "b", "c", "d", "e"], |_| vec![]),
+            Ok(vec!["a", "b", "c", "d", "e"]))
+    }
+
+    /// Sorting an array which is already in topological order, even in
+    /// the presence of dependencies, does not modify the order either.
+    #[test]
+    fn stable_deps() {
+        assert_eq!(
+            stable_topological_sort(&vec!["a", "b", "c:b", "d"], |&node| {
+                match node {
+                    "c:b" => vec!["b"],
+                    _ => vec![],
+                }
+            }),
+            Ok(vec!["a", "b", "c:b", "d"]))
+    }
+
+    /// Elements which have a dependency are sorted immediately after
+    /// their last dependency.
+    #[test]
+    fn right_after_dep() {
+        assert_eq!(
+            stable_topological_sort(&vec!["a", "b:d", "c", "d", "e"], |&node| {
+                match node {
+                    "b:d" => vec!["d"],
+                    _ => vec![],
+                }
+            }),
+            Ok(vec!["a", "c", "d", "b:d", "e"]))
+    }
+
+    /// If multiple independent elements share a dependency, their
+    /// initial order is maintained.
+    #[test]
+    fn indep_after_dep() {
+        assert_eq!(
+            stable_topological_sort(&vec!["a", "b:d", "c:d", "d", "e"], |&node| {
+                match node {
+                    "b:d" => vec!["d"],
+                    "c:d" => vec!["d"],
+                    _ => vec![],
+                }
+            }),
+            Ok(vec!["a", "d", "b:d", "c:d", "e"]))
+    }
+
+    /// If multiple elements share a dependency, their own dependencies
+    /// are still satisfied.
+    #[test]
+    fn dep_after_dep() {
+        assert_eq!(
+            stable_topological_sort(&vec!["a", "b:d,c", "c:d", "d", "e"], |&node| {
+                match node {
+                    "b:d,c" => vec!["d", "c:d"],
+                    "c:d" => vec!["d"],
+                    _ => vec![],
+                }
+            }),
+            Ok(vec!["a", "d", "c:d", "b:d,c", "e"]))
+    }
+}
+
 /// Order the choices so that they are computed in the right order to avoid overflows.
 fn order_choices<'a>(ir_desc: &'a ir::IrDesc) -> impl Iterator<Item=&'a ir::Choice> +'a {
     let names = ir_desc.choices().map(|c| c.name()).collect_vec();
-    let sorted = topological_sort(&names, |choice| {
+    let sorted = stable_topological_sort(&names, |choice| {
         let def = ir_desc.get_choice(choice).choice_def();
         if let ir::ChoiceDef::Counter { ref value, .. } = *def {
             if let ir::CounterVal::Choice(ref counter) = *value {
@@ -252,7 +513,7 @@ fn order_choices<'a>(ir_desc: &'a ir::IrDesc) -> impl Iterator<Item=&'a ir::Choi
         }
         None
     });
-    unwrap!(sorted).into_iter().rev().map(move |c| ir_desc.get_choice(c))
+    unwrap!(sorted).into_iter().map(move |c| ir_desc.get_choice(c))
 }
 
 #[derive(Debug, Serialize)]
@@ -354,7 +615,7 @@ fn inverse(enum_: &ir::Enum, f: &mut Formatter) -> fmt::Result {
 fn value_def_order<'a>(enum_: &'a ir::Enum)
         -> impl Iterator<Item=(&'a RcStr, &'a Option<String>)> +'a {
     if let Some(mapping) = enum_.inverse_mapping() {
-        let mut values: HashMap<_, _> = enum_.values().iter().collect();
+        let mut values: IndexMap<_, _> = enum_.values().iter().collect();
         Box::new(mapping.iter().flat_map(|&(ref x, ref y)| vec![x, y])
             .map(|x| (x, values.remove(x).unwrap())).collect_vec().into_iter()
             .chain(values)) as Box<Iterator<Item=_>>
