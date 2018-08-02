d42bb7b4b867bc69d0d06203d4cd76e812f5b8b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-02 13:53:16 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

--- aliases.js
+++ aliases.js
@@ -1,7 +1,13 @@
 var ALIASES = {};
 ALIASES['telamon'] = {};
+ALIASES['telamon'] = {};
+ALIASES['telamon_capi'] = {};
 ALIASES['telamon_capi'] = {};
 ALIASES['telamon_gen'] = {};
+ALIASES['telamon_gen'] = {};
+ALIASES['telamon_gen_test'] = {};
 ALIASES['telamon_gen_test'] = {};
 ALIASES['telamon_kernels'] = {};
+ALIASES['telamon_kernels'] = {};
+ALIASES['telamon_utils'] = {};
 ALIASES['telamon_utils'] = {};
