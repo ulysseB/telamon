84679125bbc14dedfa7733c4c32537bfba9afc55
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-19 23:54:06 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

---
+++ implementors/quote/to_tokens/trait.ToTokens.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["telamon_gen"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/quote/0.6.6/quote/to_tokens/trait.ToTokens.html\" title=\"trait quote::to_tokens::ToTokens\">ToTokens</a> for <a class=\"enum\" href=\"telamon_gen/ir/enum.CounterKind.html\" title=\"enum telamon_gen::ir::CounterKind\">CounterKind</a>",synthetic:false,types:["telamon_gen::ir::CounterKind"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/quote/0.6.6/quote/to_tokens/trait.ToTokens.html\" title=\"trait quote::to_tokens::ToTokens\">ToTokens</a> for <a class=\"enum\" href=\"telamon_gen/ir/enum.ValueType.html\" title=\"enum telamon_gen::ir::ValueType\">ValueType</a>",synthetic:false,types:["telamon_gen::ir::choice::ValueType"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/quote/0.6.6/quote/to_tokens/trait.ToTokens.html\" title=\"trait quote::to_tokens::ToTokens\">ToTokens</a> for <a class=\"enum\" href=\"telamon_gen/ir/enum.CmpOp.html\" title=\"enum telamon_gen::ir::CmpOp\">CmpOp</a>",synthetic:false,types:["telamon_gen::ir::filter::CmpOp"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
