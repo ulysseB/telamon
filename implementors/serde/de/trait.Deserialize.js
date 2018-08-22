a00871f672862ee2a85e5a05b4ef42024c3db4f1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-22 08:10:02 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

--- implementors/serde/de/trait.Deserialize.js
+++ implementors/serde/de/trait.Deserialize.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["telamon_utils"] = [{text:"impl&lt;'de, T:&nbsp;<a class=\"trait\" href=\"https://docs.rs/serde/1.0.71/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt;&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.71/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"telamon_utils/sequence/enum.Sequence.html\" title=\"enum telamon_utils::sequence::Sequence\">Sequence</a>&lt;T&gt;",synthetic:false,types:["telamon_utils::sequence::Sequence"]},];
+implementors["telamon_utils"] = [{text:"impl&lt;'de, T:&nbsp;<a class=\"trait\" href=\"https://docs.rs/serde/1.0.72/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt;&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.72/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"telamon_utils/sequence/enum.Sequence.html\" title=\"enum telamon_utils::sequence::Sequence\">Sequence</a>&lt;T&gt;",synthetic:false,types:["telamon_utils::sequence::Sequence"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
