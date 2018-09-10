a430468380a6278c7c6bc57c8f18fa45391d02f6
Nicolas Tollenaere <nicolas.tollenaere@inria.com>
2018-09-10 11:30:21 -0400

    Created a distinct file for Choice

--- telamon-gen/src/print/template/actions.rs
+++ telamon-gen/src/print/template/actions.rs
@@ -37,25 +37,3 @@ pub fn apply_action(action: Action, store: &mut DomainStore, diff: &mut DomainDi
         {{~/each}}
     }
 }
-
-#[derive(PartialEq, Eq, Debug, Clone, Copy, Serialize, Deserialize)]
-#[repr(C)]
-pub enum Choice {
-    {{~#each choices}}
-        /// cbindgen:field-names=[{{~#each arguments}}{{this.[0]}}, {{/each~}}domain]
-        {{to_type_name name}}(
-        {{~#each arguments}}{{this.[1].def.keys.IdType}},{{/each~}}
-        ),
-    {{/each~}}
-}
-
-impl From<Action> for Choice {
-    fn from(action: Action) -> Choice {
-        match action {
-            {{~#each choices}}
-                Action::{{to_type_name name}}(
-                {{#each arguments}}{{this.[0]}},{{~/each}} _ ) => Choice::{{to_type_name name}}({{#each arguments}}{{this.[0]}}, {{~/each}}),
-            {{~/each}}
-        }
-    }
-}
---
+++ telamon-gen/src/print/template/choices.rs
@@ -0,0 +1,21 @@
+#[derive(PartialEq, Eq, Debug, Clone, Copy, Serialize, Deserialize)]
+#[repr(C)]
+pub enum Choice {
+    {{~#each choices}}
+        /// cbindgen:field-names=[{{~#each arguments}}{{this.[0]}}, {{/each~}}domain]
+        {{to_type_name name}}(
+        {{~#each arguments}}{{this.[1].def.keys.IdType}},{{/each~}}
+        ),
+    {{/each~}}
+}
+
+impl From<Action> for Choice {
+    fn from(action: Action) -> Choice {
+        match action {
+            {{~#each choices}}
+                Action::{{to_type_name name}}(
+                {{#each arguments}}{{this.[0]}},{{~/each}} _ ) => Choice::{{to_type_name name}}({{#each arguments}}{{this.[0]}}, {{~/each}}),
+            {{~/each}}
+        }
+    }
+}
