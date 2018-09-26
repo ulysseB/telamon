73bb32dbe8051e929ba32be1d7529b60a9bc4b1c
Ulysse Beaugnon <ulysse.beaugnon@ens.fr>
2018-09-26 08:31:06 -0700

    fix stable output

--- telamon-gen/src/lib.rs
+++ telamon-gen/src/lib.rs
@@ -160,37 +160,23 @@ pub fn process<T: io::Write>(
 #[cfg(test)]
 mod tests {
     use super::print;
-    use std::io::Cursor;
     use std::path::Path;
 
     /// Ensure that the output of telamon-gen is stable across calls.
     #[test]
     fn stable_output() {
-        let mut in_buf =
-            Cursor::new(include_str!("../../src/search_space/choices.exh").as_bytes());
+        let path = Path::new("../src/search_space/choices.exh");
         let ref_out = {
             let mut ref_out = Vec::new();
-            super::process(
-                Some(&mut in_buf),
-                &mut ref_out,
-                false,
-                &Path::new("choices.exh"),
-            ).unwrap();
+            super::process( None, &mut ref_out, false, &path).unwrap();
             ref_out
         };
         // Ideally we would want to run this loop more than once, but
         // generation is currently too slow to be worth it.
         for _ in 0..1 {
             print::reset();
-            in_buf.set_position(0);
-
             let mut out_buf = Vec::new();
-            super::process(
-                Some(&mut in_buf),
-                &mut out_buf,
-                false,
-                &Path::new("choices.exh"),
-            ).unwrap();
+            super::process( None, &mut out_buf, false, &path).unwrap();
             assert_eq!(
                 ::std::str::from_utf8(&out_buf),
                 ::std::str::from_utf8(&ref_out)
