7b3435752604bf228cc531234f331d00e55547cc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-05 15:05:30 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

---
+++ implementors/core/str/trait.FromStr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["telamon"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"telamon/explorer/config/enum.ChoiceGroup.html\" title=\"enum telamon::explorer::config::ChoiceGroup\">ChoiceGroup</a>",synthetic:false,types:["telamon::explorer::config::ChoiceGroup"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"telamon/explorer/config/struct.ChoiceOrdering.html\" title=\"struct telamon::explorer::config::ChoiceOrdering\">ChoiceOrdering</a>",synthetic:false,types:["telamon::explorer::config::ChoiceOrdering"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["telamon"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"telamon/explorer/config/struct.ParseChoiceGroupError.html\" title=\"struct telamon::explorer::config::ParseChoiceGroupError\">ParseChoiceGroupError</a>",synthetic:false,types:["telamon::explorer::config::ParseChoiceGroupError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
