4a1e075739020238cb99e8213b755b99ffef51bd
Nicolas Tollenaere <nicolas.tollenaere@inria.fr>
2018-11-05 16:28:07 +0100

    Using config path in bench

--- kernels/benches/test_cut.rs
+++ kernels/benches/test_cut.rs
@@ -1,18 +1,31 @@
 //! Benchmarks the accuracy of bounds on CUDA GPUs.
 extern crate env_logger;
+extern crate serde_json;
 extern crate telamon;
 extern crate telamon_kernels;
 extern crate telamon_utils;
+extern crate xdg;
 
 use telamon::device::{cuda, fake::FakeContext};
 use telamon_kernels::{linalg, Kernel};
 use telamon_utils::*;
 
+/// Returns the name of the configuration file.
+pub fn get_config_path() -> std::path::PathBuf {
+    let xdg_dirs = unwrap!(xdg::BaseDirectories::with_prefix("telamon"));
+    // We use `place_config_file` instead of `find_config_file` to avoid returning
+    // a system-wide files (e.g. in /usr/share/telamon/cuda_gpus.json) on which the user
+    // doesn't have write permissions.
+    let path = xdg_dirs.place_config_file("cuda_gpus.json");
+    unwrap!(path, "cannot create configuration directory")
+}
+
 macro_rules! test_cut {
     ($name:ident, $kernel:ty, $params:expr) => {
         fn $name() {
             let _ = env_logger::try_init();
-            let gpu = unwrap!(cuda::characterize::get_gpu_desc_from_file());
+            //let gpu = unwrap!(cuda::characterize::get_gpu_desc_from_file());
+            let gpu: cuda::Gpu = unwrap!(serde_json::from_reader(&unwrap!(std::fs::File::open(get_config_path()))));
             let mut context = FakeContext::new(gpu);
             <$kernel>::find_cut_depth($params, 1.0e4, &mut context);
         }
