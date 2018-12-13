1e1b33fd6fdd80c60636a0399650d427691b8862
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-13 12:28:32 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["telamon"] = [{text:"impl Fail for <a class=\"enum\" href=\"telamon/ir/enum.TypeError.html\" title=\"enum telamon::ir::TypeError\">TypeError</a>",synthetic:false,types:["telamon::ir::error::TypeError"]},{text:"impl Fail for <a class=\"enum\" href=\"telamon/ir/enum.Error.html\" title=\"enum telamon::ir::Error\">Error</a>",synthetic:false,types:["telamon::ir::error::Error"]},];
+implementors["telamon_capi"] = [{text:"impl Fail for <a class=\"enum\" href=\"telamon_capi/error/enum.Error.html\" title=\"enum telamon_capi::error::Error\">Error</a>",synthetic:false,types:["telamon_capi::error::Error"]},];
+implementors["telamon_gen"] = [{text:"impl Fail for <a class=\"enum\" href=\"telamon_gen/ast/enum.TypeError.html\" title=\"enum telamon_gen::ast::TypeError\">TypeError</a>",synthetic:false,types:["telamon_gen::ast::error::TypeError"]},{text:"impl Fail for <a class=\"enum\" href=\"telamon_gen/lexer/enum.ErrorKind.html\" title=\"enum telamon_gen::lexer::ErrorKind\">ErrorKind</a>",synthetic:false,types:["telamon_gen::lexer::ErrorKind"]},{text:"impl Fail for <a class=\"struct\" href=\"telamon_gen/lexer/struct.LexicalError.html\" title=\"struct telamon_gen::lexer::LexicalError\">LexicalError</a>",synthetic:false,types:["telamon_gen::lexer::LexicalError"]},{text:"impl Fail for <a class=\"struct\" href=\"telamon_gen/error/struct.Error.html\" title=\"struct telamon_gen::error::Error\">Error</a>",synthetic:false,types:["telamon_gen::error::Error"]},];
+implementors["telamon_utils"] = [{text:"impl Fail for <a class=\"enum\" href=\"telamon_utils/tfrecord/enum.ReadError.html\" title=\"enum telamon_utils::tfrecord::ReadError\">ReadError</a>",synthetic:false,types:["telamon_utils::tfrecord::ReadError"]},{text:"impl Fail for <a class=\"enum\" href=\"telamon_utils/tfrecord/enum.WriteError.html\" title=\"enum telamon_utils::tfrecord::WriteError\">WriteError</a>",synthetic:false,types:["telamon_utils::tfrecord::WriteError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
