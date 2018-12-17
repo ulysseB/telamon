ffd9280606e3cb2f4a43aa5a83d73050c7204fb6
Basile Clement <basile.clement@ens.fr>
2018-12-17 17:40:26 +0100

    Add a CLI tool to extract data from event logs
    The new tool, in `examples/parse_event_log`, allows dumping a CSV of all
    the evaluations that were performed during a run, as well as a graphviz
    (.dot) graph recapitulating the run.
    
    telamon-utils/
     - src/tfrecord.rs:  Add `.records()` iterator method.
    
    telamon/
     - Cargo.toml:  Add crates for graphviz and csv writing.
     - examples/print_event_log.rs:  Upgrade to the `.records()` API
     - examples/parse_event_log.rs:  Added.

--- examples/print_event_log.rs
+++ examples/print_event_log.rs
@@ -1,3 +1,4 @@
+/// This example shows how to iterate over the events stored in an eventlog.
 extern crate bincode;
 extern crate telamon;
 extern crate telamon_utils as utils;
@@ -9,30 +10,15 @@ use utils::tfrecord::{ReadError, RecordReader};
 
 fn main() -> Result<(), ReadError> {
     let mut f = std::fs::File::open("eventlog.tfrecord")?;
-    let mut offset;
-    loop {
-        offset = f.seek(io::SeekFrom::Current(0))?;
 
-        match f.read_record() {
-            Ok(record) => match bincode::deserialize(&record).unwrap() {
-                TreeEvent::Evaluation {
-                    actions,
-                    score: _score,
-                } => println!("{:?}", actions.into_iter().collect::<Vec<_>>()),
-            },
-            Err(err) => {
-                // If we reached eof and no bytes were read, we were
-                // at the end of a well-formed file and we can safely
-                // exit. Otherwise, we propagate the error.
-                if let ReadError::IOError(ref error) = err {
-                    if error.kind() == io::ErrorKind::UnexpectedEof
-                        && offset == f.seek(io::SeekFrom::Current(0))?
-                    {
-                        return Ok(());
-                    }
-                }
-                return Err(err);
-            }
+    for record_bytes in (&mut f).records() {
+        match bincode::deserialize(&record_bytes?).unwrap() {
+            TreeEvent::Evaluation {
+                actions,
+                score: _score,
+            } => println!("{:?}", actions.into_iter().collect::<Vec<_>>()),
         }
     }
+
+    Ok(())
 }
