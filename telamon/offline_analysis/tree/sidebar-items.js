6adaab020a4904e29590318a23b668e53da5b936
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-21 17:09:50 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

---
+++ telamon/offline_analysis/tree/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["BoundGuard","Proxy for bound, capturing bound from CandidateNode::bound()"],["CandidateNode","A node of in the resconstructed candidate tree"],["CandidateTree","A reconstructed tree"],["ParentEdge","Edge to parent with `child_idx` indicating the index within the list of children of `parent`."]]});
