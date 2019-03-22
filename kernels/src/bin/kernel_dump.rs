d54cd31b1f9461951ef92798338893024d31bd81
Nicolas Tollenaere <nicolas.tollenaere@inria.fr>
2019-03-22 09:55:15 +0100

    Now skipping dump generation when file exists

--- kernels/src/bin/kernel_dump.rs
+++ kernels/src/bin/kernel_dump.rs
@@ -1,3 +1,4 @@
+use log::warn;
 use std::{fs, path::Path};
 use telamon_kernels::{linalg, Kernel};
 use telamon_x86 as x86;
@@ -6,18 +7,15 @@ macro_rules! kernel_dump {
     ($kernel:ty, $num_tests:expr, $params:expr) => {
         let _ = env_logger::try_init();
         let mut context = x86::Context::default();
-        let mut path = format!("kernel_dump/x86/{}.dump", <$kernel>::name());
-        let mut increment = 0;
-        loop {
-            if Path::new(&path).exists() {
-                increment += 1;
-                path =
-                    format!("kernel_dump/x86/{}_{}.dump", <$kernel>::name(), increment);
-            } else {
-                let mut file = fs::File::create(path).unwrap();
-                <$kernel>::generate_dump($params, &mut context, &mut file);
-                break;
-            }
+        let path = format!("kernel_dump/x86/{}.dump", <$kernel>::name());
+        if !Path::new(&path).exists() {
+            let mut file = fs::File::create(path).unwrap();
+            <$kernel>::generate_dump($params, &mut context, &mut file);
+        } else {
+            warn!(
+                "Skipping generation of {} as a dump already exists",
+                <$kernel>::name()
+            );
         }
     };
 }
