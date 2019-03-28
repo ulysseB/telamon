13a440de8d9235bfa1eb6376c361b23e3851080c
Nicolas Tollenaere <nicolas.tollenaere@inria.fr>
2019-03-28 15:01:08 +0100

    Removed macro

--- kernels/src/bin/exec_dump.rs
+++ kernels/src/bin/exec_dump.rs
@@ -2,18 +2,6 @@ use std::env;
 use telamon_kernels::{linalg, Kernel};
 use telamon_x86 as x86;
 
-macro_rules! dispatch_kernel {
-    ($name:expr, $func_call:ident, $( $arg:expr),* ) => {
-        match $name {
-            "axpy" => linalg::Axpy::<f32>::$func_call($($arg),*),
-            "mv" => linalg::MatVec::<f32>::$func_call($($arg),*),
-            "gesummv" => linalg::Gesummv::<f32>::$func_call($($arg),*),
-            "matmul" => linalg::MatMul::<f32>::$func_call($($arg),*),
-            _ => panic!("Valid kernel names are: axpy, mv, gesummv, matmul"),
-        }
-    };
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() == 3);
@@ -24,5 +12,11 @@ fn main() {
     let mut context = x86::Context::default();
 
     let mut file = std::fs::File::open(binary_path).expect("Invalid dump path");
-    dispatch_kernel!(kernel_name as &str, execute_dump, &mut context, &mut file);
+    match kernel_name as &str {
+        "axpy" => linalg::Axpy::<f32>::execute_dump(&mut context, &mut file),
+        "mv" => linalg::MatVec::<f32>::execute_dump(&mut context, &mut file),
+        "gesummv" => linalg::Gesummv::<f32>::execute_dump(&mut context, &mut file),
+        "matmul" => linalg::MatMul::<f32>::execute_dump(&mut context, &mut file),
+        _ => panic!("Valid kernel names are: axpy, mv, gesummv, matmul"),
+    }
 }
