b6e73d86e5020e0ce8e5ca2f749891aff96b2bad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-11 16:55:20 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

--- implementors/core/iter/traits/trait.Product.js
+++ implementors/core/iter/traits/trait.Product.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["telamon"] = [{text:"impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Product.html\" title=\"trait core::iter::traits::Product\">Product</a>&lt;&amp;'b <a class=\"struct\" href=\"telamon/ir/struct.PartialSize.html\" title=\"struct telamon::ir::PartialSize\">PartialSize</a>&lt;'a&gt;&gt; for <a class=\"struct\" href=\"telamon/ir/struct.PartialSize.html\" title=\"struct telamon::ir::PartialSize\">PartialSize</a>&lt;'a&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;'a: 'b,&nbsp;</span>",synthetic:false,types:["telamon::ir::size::PartialSize"]},];
+implementors["telamon"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Product.html\" title=\"trait core::iter::traits::Product\">Product</a>&lt;&amp;'a <a class=\"struct\" href=\"telamon/ir/struct.PartialSize.html\" title=\"struct telamon::ir::PartialSize\">PartialSize</a>&gt; for <a class=\"struct\" href=\"telamon/ir/struct.PartialSize.html\" title=\"struct telamon::ir::PartialSize\">PartialSize</a>",synthetic:false,types:["telamon::ir::size::PartialSize"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/ops/arith/trait.Mul.js
+++ implementors/core/ops/arith/trait.Mul.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["telamon"] = [{text:"impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;&amp;'b <a class=\"struct\" href=\"telamon/ir/struct.PartialSize.html\" title=\"struct telamon::ir::PartialSize\">PartialSize</a>&lt;'a&gt;&gt; for <a class=\"struct\" href=\"telamon/ir/struct.PartialSize.html\" title=\"struct telamon::ir::PartialSize\">PartialSize</a>&lt;'a&gt;",synthetic:false,types:["telamon::ir::size::PartialSize"]},];
+implementors["telamon"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;&amp;'a <a class=\"struct\" href=\"telamon/ir/struct.PartialSize.html\" title=\"struct telamon::ir::PartialSize\">PartialSize</a>&gt; for <a class=\"struct\" href=\"telamon/ir/struct.PartialSize.html\" title=\"struct telamon::ir::PartialSize\">PartialSize</a>",synthetic:false,types:["telamon::ir::size::PartialSize"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
