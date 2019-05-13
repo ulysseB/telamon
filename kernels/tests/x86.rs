8517fb75a624d9594e8f788ed80528c562b78b95
Andi Drebes <andi@drebesium.org>
2019-05-13 14:26:30 +0200

    Use JSON for kernel dumps
    Using JSON instead of a binary format for kernel dumps makes debugging
    easier and allows for the inspection of dumps even when the data
    structures have changed.

--- kernels/tests/x86.rs
+++ kernels/tests/x86.rs
@@ -7,7 +7,7 @@ macro_rules! test_dump {
         fn $name() {
             let _ = env_logger::try_init();
             let mut context = x86::Context::default();
-            let path = format!("kernel_dump/x86/{}.dump", stringify!($name));
+            let path = format!("kernel_dump/x86/{}.json", stringify!($name));
             let mut file = std::fs::File::open(&path).unwrap();
             <$kernel>::execute_dump(&mut context, &mut file);
         }
