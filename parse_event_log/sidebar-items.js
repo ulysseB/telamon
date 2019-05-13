5f8b1cc9fcb78316eae88a165c0ab381098e04c1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-13 17:37:59 +0000

    Deploy ulysseB/telamon to github.com/ulysseB/telamon.git:gh-pages

--- parse_event_log/sidebar-items.js
+++ parse_event_log/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({});
+initSidebarItems({"enum":[["DeadEndSource",""],["TreeEvent","The possible tree events. WARNING:  Changing the enums will break any pre-existing eventlog files.  Adding new cases at the end only is safe."]]});
