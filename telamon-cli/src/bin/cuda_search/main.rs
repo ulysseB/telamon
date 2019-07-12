864184014a65ce306e825cafa1a63593e8c001c4
Basile Clement <basile.clement@ens.fr>
2019-07-12 12:10:28 +0200

    Add a telamon-cli package with CLI utilities
    The telamon-cli package currently provides two executables:
    
     - A `cuda_search` executable which runs the search for a given kernel
       and configuration
    
     - A `tlcli` swiss knife executable which allows doing a bunch of handy
       and common tasks such as extracting lists of actions from event log,
       displaying eventlog statistics, replaying a list of action, etc.
       Those are scripts I had lying around that I find useful, and as such
       worth it to share.
    
    (Yes, those should be binaries in the main `telamon` package.  They are
    not because (a) it would require recompiling the main `telamon` package
    each time, which is way too slow and (b) they depend on the kernels.
    Yes, this should be fixed; but it requires a non negligible amount of
    refactoring -- so in the meantime let's use a separate crate)

---
+++ telamon-cli/src/bin/cuda_search/main.rs
@@ -0,0 +1,188 @@
+//! Benchmarks the exploration on CUDA gpus.
+use std::io::{self, Write};
+
+use telamon::device::{ArgMap, Context};
+use telamon::explorer::config::Config;
+use telamon::helper::MemInit;
+use telamon_cli::{
+    Bench, CommonOpt, ContextBuilder, CublasHandle, KernelParam, Reference,
+};
+use telamon_kernels::statistics::estimate_mean;
+use telamon_kernels::{linalg, Kernel};
+
+use structopt::StructOpt;
+
+/// The number of times to run the generated code to evaluate its performance.
+const NUM_CODE_RUNS: usize = 40;
+/// Search timeout in minutes.
+const TIMEOUT: u64 = 240;
+
+/// Benchamrks a kernel against a reference implementation.
+fn benchmark<'a, K, REF, CB>(
+    mut config: Config,
+    params: K::Parameters,
+    executor: CB,
+    reference: &REF,
+    // output_dir: String,
+) where
+    K: Kernel<'a>,
+    CB: ContextBuilder<'a>,
+    REF: Reference<'a, K, Context = CB::Context>,
+{
+    config.timeout.get_or_insert(TIMEOUT);
+    // config.output_dir = output_dir;
+    //config.distance_to_best.get_or_insert(20.);
+
+    let mut context = executor.build_context();
+    let runtime = K::benchmark(
+        &config,
+        params.clone(),
+        NUM_CODE_RUNS,
+        MemInit::RandomFill,
+        &mut context,
+    );
+    let ref_runtime = Bench::default()
+        .warmup(4)
+        .runs(NUM_CODE_RUNS)
+        .benchmark_fn(|| reference.eval_reference(&params, &context));
+    let mut f =
+        std::fs::File::create(config.output_path("benchmark.txt").unwrap()).unwrap();
+    writeln!(f, "runtimes: {:?}", runtime).unwrap();
+    let mean = estimate_mean(runtime, 0.95, "ns");
+    let ref_mean = estimate_mean(ref_runtime, 0.95, "ns");
+    writeln!(
+        f,
+        "{}: {}, reference: {}, speedup: {:.2}",
+        K::name(),
+        mean,
+        ref_mean,
+        ref_mean.value / mean.value
+    )
+    .unwrap();
+}
+
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(flatten)]
+    common: CommonOpt,
+
+    #[structopt(short = "r", long = "repeat", default_value = "10")]
+    repeat: usize,
+
+    #[structopt(short = "k", long = "kernel")]
+    kernels: Vec<KernelParam>,
+}
+
+trait BenchRun<'a, B, R> {
+    fn run(self, config: &Config, builder: B, reference: &R);
+}
+
+struct Benchmark<'a, K>
+where
+    K: Kernel<'a>,
+{
+    params: K::Parameters,
+    name: String,
+    iteration: usize,
+}
+
+impl<'a, K> Benchmark<'a, K>
+where
+    K: Kernel<'a>,
+{
+    fn new(params: K::Parameters, name: String, iteration: usize) -> Self {
+        Benchmark {
+            params,
+            name,
+            iteration,
+        }
+    }
+
+    fn output_dir(&self) -> String {
+        format!("{}/{}", self.name, self.iteration)
+    }
+}
+
+impl<'a, K, B, R> BenchRun<'a, B, R> for Benchmark<'a, K>
+where
+    K: Kernel<'a>,
+    B: ContextBuilder<'a>,
+    R: Reference<'a, K, Context = B::Context>,
+{
+    fn run(self, config: &Config, builder: B, reference: &R) {
+        let mut config = config.clone();
+        config.output_dir = std::path::Path::new(&config.output_dir)
+            .join(self.output_dir())
+            .to_str()
+            .unwrap()
+            .to_string();
+        benchmark::<K, _, _>(config.clone(), self.params, builder, reference)
+    }
+}
+
+macro_rules! benchmark {
+    (Sgemm($m:expr, $n:expr, $k:expr)[$iter:expr]) => {{
+        self::Benchmark::<'_, linalg::FusedMM<'_, f32>>::new(
+            linalg::FusedMMP::new($m, $n, $k),
+            format!("Sgemm_{}_{}_{}", $m, $n, $k),
+            $iter,
+        )
+    }};
+
+    (BatchMM($b:expr, $m:expr, $n:expr, $k:expr)[$iter:expr]) => {{
+        self::Benchmark::<'_, linalg::BatchMM<'_, f32>>::new(
+            linalg::BatchMMP::new($b, $m, $n, $k),
+            format!("BatchMM_{}_{}_{}_{}", $b, $m, $n, $k),
+            $iter,
+        )
+    }};
+}
+
+fn main() {
+    env_logger::init();
+    let args = Opt::from_args();
+
+    let executor = telamon_cuda::Executor::init();
+    let reference = CublasHandle::new();
+
+    let config = args.common.config().unwrap();
+
+    for idx in 0..args.repeat {
+        for kernel in &args.kernels {
+            use KernelParam::*;
+
+            match *kernel {
+                Axpy { n } => Benchmark::<'_, linalg::Axpy<f32>>::new(
+                    (n, true),
+                    format!("Axpy_{}", n),
+                    idx,
+                )
+                .run(&config, &executor, &reference),
+                MatVec { m, n } => Benchmark::<'_, linalg::MatVec<f32>>::new(
+                    (m, n, true),
+                    format!("Sgemv_{}_{}", m, n),
+                    idx,
+                )
+                .run(&config, &executor, &reference),
+                Gesummv { m, n } => Benchmark::<'_, linalg::Gesummv<f32>>::new(
+                    (m, n, true),
+                    format!("Gesummv_{}_{}", m, n),
+                    idx,
+                )
+                .run(&config, &executor, &reference),
+                Gemm { m, n, k } => Benchmark::<'_, linalg::FusedMM<'_, f32>>::new(
+                    linalg::FusedMMP::new(m, n, k),
+                    format!("Sgemm_{}_{}_{}", m, n, k),
+                    idx,
+                )
+                .run(&config, &executor, &reference),
+                BatchMM { b, m, n, k } => Benchmark::<'_, linalg::BatchMM<'_, f32>>::new(
+                    linalg::BatchMMP::new(b, m, n, k),
+                    format!("BatchMM_{}_{}_{}_{}", b, m, n, k),
+                    idx,
+                )
+                .run(&config, &executor, &reference),
+            }
+        }
+    }
+}
---
+++ telamon-cli/src/bin/tlcli.rs
@@ -0,0 +1,458 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
+use std::io::{self, Write};
+use std::path::PathBuf;
+use std::sync::{atomic, Arc, Mutex};
+
+use serde_json;
+use structopt::StructOpt;
+
+use telamon::device;
+use telamon::explorer::{
+    self,
+    choice::{ActionEx as Action, Choice},
+    config,
+    eventlog::EventLog,
+    mcts, Candidate,
+};
+use telamon::ir::IrDisplay;
+use telamon::model::{bound, Bound};
+use telamon::offline_analysis::tree::CandidateTree;
+use telamon::search_space::SearchSpace;
+
+use telamon_cli::{KernelParam, ReplayPath};
+
+/// Compute the bound for a given candidate.
+#[derive(StructOpt)]
+struct ComputeBound {
+    /// Kernel specification to use.
+    #[structopt(short = "k", long = "kernel")]
+    kernel: KernelParam,
+
+    /// Path to a saved replay file to load before computing the bound.
+    #[structopt(parse(from_os_str), short = "r", long = "replay")]
+    replay: Option<ReplayPath>,
+}
+
+impl ComputeBound {
+    fn run(&self, _args: &Opt) -> io::Result<()> {
+        let executor = telamon_cuda::Executor::init();
+        let mut context = telamon_cuda::Context::new(&executor);
+        let (mut candidates, context) = self.kernel.build(&mut context);
+        assert!(candidates.len() == 1);
+        let mut candidate = candidates.swap_remove(0).space;
+
+        // Apply replay if there is some
+        if let Some(replay) = &self.replay {
+            for action in &replay.load()? {
+                candidate = action
+                    .apply_to(candidate)
+                    .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+            }
+        }
+
+        let start = std::time::Instant::now();
+        let bound = bound(&candidate, context);
+        let duration = start.elapsed();
+        println!("Bound is {:?} (computed in {:?})", bound, duration);
+
+        Ok(())
+    }
+}
+
+/// Compute bounds.csv
+#[derive(StructOpt)]
+struct Bounds {
+    #[structopt(long = "order")]
+    order: Option<config::ChoiceOrdering>,
+
+    #[structopt(long = "kernel")]
+    kernel: KernelParam,
+
+    #[structopt(long = "num-runs", default_value = "500")]
+    num_runs: usize,
+}
+
+/// Ignore candidates with a too big bound in tests.
+const CUT: f64 = 2e8f64;
+
+impl Bounds {
+    fn next_choice(&self, space: &SearchSpace) -> Option<Choice> {
+        if let Some(order) = &self.order {
+            explorer::choice::list(order, space).next()
+        } else {
+            explorer::choice::default_list(space).next()
+        }
+    }
+
+    /// Descend along a path in the search tree and stores the bounds encountered on the way.
+    fn random_descent(
+        &self,
+        candidates: &[Candidate],
+        context: &dyn device::Context,
+    ) -> Option<(Candidate, Vec<Bound>)> {
+        let order = explorer::config::NewNodeOrder::Random;
+        let mut candidates = Cow::Borrowed(candidates);
+        let mut bounds = Vec::new();
+        loop {
+            let idx = if let Some(idx) = order.pick_candidate(&candidates, CUT) {
+                idx
+            } else {
+                break None;
+            };
+            bounds.push(candidates[idx].bound.clone());
+            let choice_opt = self.next_choice(&candidates[idx].space);
+            if let Some(choice) = choice_opt {
+                let new_nodes = candidates[idx]
+                    .apply_choice(context, choice)
+                    .into_iter()
+                    .filter(|x| x.bound.value() < CUT)
+                    .collect::<Vec<_>>();
+                candidates = std::borrow::Cow::Owned(new_nodes);
+            } else {
+                break Some((
+                    match candidates {
+                        Cow::Borrowed(candidates) => candidates[idx].clone(),
+                        Cow::Owned(mut candidates) => candidates.swap_remove(idx),
+                    },
+                    bounds,
+                ));
+            }
+        }
+    }
+
+    fn test_bound<F>(
+        &self,
+        candidates: Vec<Candidate>,
+        context: &dyn device::Context,
+        body_fn: F,
+    ) where
+        F: Fn((f64, Vec<f64>)) + Sync,
+    {
+        let num_tested = atomic::AtomicUsize::new(0);
+        let stabilizer = &context.stabilizer();
+        context.async_eval(
+            num_cpus::get(),
+            device::EvalMode::TestBound,
+            &|evaluator| loop {
+                if num_tested.fetch_add(1, atomic::Ordering::SeqCst) >= self.num_runs {
+                    if num_tested.fetch_sub(1, atomic::Ordering::SeqCst) > self.num_runs {
+                        break;
+                    }
+                }
+
+                if let Some((leaf, mut bounds)) =
+                    self.random_descent(&candidates, context)
+                {
+                    evaluator.add_kernel(leaf, {
+                        let body_fn = &body_fn;
+                        move |leaf, kernel| {
+                            let bound = leaf.bound.clone();
+                            let runtime = stabilizer
+                                .wrap(kernel)
+                                .bound(Some(bound.value()))
+                                .evaluate()
+                                .unwrap();
+                            bounds.push(bound);
+                            body_fn((
+                                runtime,
+                                bounds.into_iter().map(|bound| bound.value()).collect(),
+                            ))
+                        }
+                    });
+                } else {
+                    num_tested.fetch_sub(1, atomic::Ordering::SeqCst);
+                }
+            },
+        );
+    }
+
+    fn run(&self, _args: &Opt) -> io::Result<()> {
+        let executor = telamon_cuda::Executor::init();
+        let mut context = telamon_cuda::Context::new(&executor);
+        let (candidates, context) = self.kernel.build(&mut context);
+        let stdout = std::io::stdout();
+        self.test_bound(candidates, context, |(runtime, bounds)| {
+            let mut handle = stdout.lock();
+            write!(handle, "{},{}", self.kernel, runtime).unwrap();
+            for bound in bounds {
+                write!(handle, ",{}", bound).unwrap();
+            }
+            write!(handle, "\n").unwrap();
+        });
+
+        Ok(())
+    }
+}
+
+/// Rebuild a specific list of actions from an event log.
+///
+/// This generates a .json replay file which can be used by the debugger, as well as by various
+/// other utilities (eg the `compute_bound` subcommand).
+#[derive(StructOpt)]
+struct Rebuild {
+    /// Path to the eventlog to rebuild from
+    #[structopt(
+        parse(from_os_str),
+        short = "i",
+        long = "input",
+        default_value = "eventlog.tfrecord.gz"
+    )]
+    eventlog: PathBuf,
+
+    /// Identifier of the candidate node to rebuild.  This corresponds to the ID indicated in
+    /// `watch.log`.
+    id: usize,
+}
+
+impl Rebuild {
+    fn find_candidate(&self) -> io::Result<(mcts::NodeId, f64, Vec<Action>)> {
+        let mut nevals = 0;
+        let mut tree = CandidateTree::new();
+
+        for record_bytes in EventLog::open(&self.eventlog)?.records() {
+            match bincode::deserialize(&record_bytes?)
+                .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?
+            {
+                mcts::Message::Node {
+                    id,
+                    parent,
+                    mut children,
+                    bound,
+                    discovery_time,
+                } => tree.extend(id, discovery_time, parent, bound, &mut children),
+                mcts::Message::Trace { .. } => (),
+                mcts::Message::Evaluation { id, value, .. } => {
+                    if let Some(value) = value {
+                        if nevals == self.id {
+                            return Ok((id, value, tree.get_node(id).actions()));
+                        }
+
+                        nevals += 1;
+                    }
+                }
+            }
+        }
+
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Unable to find candidate",
+        ))
+    }
+
+    fn run(&self, _args: &Opt) -> io::Result<()> {
+        let (id, score, actions) = self.find_candidate()?;
+
+        println!("Found candidate {} (score: {})", id, score);
+        println!("{}", serde_json::to_string_pretty(&actions)?);
+
+        Ok(())
+    }
+}
+
+/// Compute statistics on an eventlog
+#[derive(StructOpt)]
+struct Stats {
+    /// Path to the eventlog to compute stats for
+    #[structopt(
+        parse(from_os_str),
+        short = "i",
+        long = "input",
+        default_value = "eventlog.tfrecord.gz"
+    )]
+    eventlog: PathBuf,
+
+    /// Maximum number of implementations to consider
+    #[structopt(long = "limit")]
+    limit: Option<usize>,
+}
+
+impl Stats {
+    fn run(&self, _args: &Opt) -> io::Result<()> {
+        let (mut nimpl, mut impld) = (0, 0u64);
+
+        #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+        enum Cause {
+            Constraints,
+            PerfModel,
+            Backtrack,
+        };
+
+        impl From<mcts::CauseOfDeath> for Cause {
+            fn from(cause: mcts::CauseOfDeath) -> Self {
+                match cause {
+                    mcts::CauseOfDeath::Constraints => Cause::Constraints,
+                    mcts::CauseOfDeath::PerfModel { .. } => Cause::PerfModel,
+                    mcts::CauseOfDeath::Backtrack => Cause::Backtrack,
+                }
+            }
+        }
+
+        let mut deadinfo = HashMap::new();
+
+        let mut evalns = self.limit.map(Vec::with_capacity).unwrap_or_default();
+        let mut tree = CandidateTree::new();
+
+        for record_bytes in EventLog::open(&self.eventlog)?.records() {
+            match bincode::deserialize(&record_bytes?)
+                .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?
+            {
+                mcts::Message::Node {
+                    id,
+                    parent,
+                    mut children,
+                    bound,
+                    discovery_time,
+                } => tree.extend(id, discovery_time, parent, bound, &mut children),
+                mcts::Message::Trace { events, .. } => {
+                    let mut cause = None;
+                    let mut len = 0;
+                    let mut node = tree.get_root();
+                    let mut has_size = false;
+
+                    for event in &events {
+                        match event.value {
+                            mcts::Event::SelectNode(id) => {
+                                node = tree.get_node(id);
+                            }
+                            mcts::Event::SelectChild(index, ..) => {
+                                node = node
+                                    .child(index.into())
+                                    .unwrap_or_else(|| panic!("no child"));
+                                match node.action().unwrap_or_else(|| panic!("no action"))
+                                {
+                                    Action::Action(
+                                        telamon::search_space::Action::Size(..),
+                                    ) => has_size = true,
+                                    _ => (),
+                                }
+                                len += 1;
+                            }
+                            mcts::Event::KillChild(index, cause_) => {
+                                let info = deadinfo
+                                    .entry((Cause::from(cause_), has_size))
+                                    .or_insert((0u64, 0u32));
+                                info.0 += len + 1;
+                                info.1 += 1;
+                            }
+                            mcts::Event::Kill(cause_) => {
+                                assert!(cause.is_none());
+
+                                cause = Some(Cause::from(cause_));
+                            }
+                            mcts::Event::Implementation => {
+                                assert!(cause.is_none());
+
+                                impld += len;
+                                nimpl += 1;
+                            }
+                            mcts::Event::Expand => (),
+                        }
+                    }
+
+                    if let Some(cause) = cause {
+                        let info =
+                            deadinfo.entry((cause, has_size)).or_insert((0u64, 0u32));
+                        info.0 += len;
+                        info.1 += 1;
+                    }
+                }
+                mcts::Message::Evaluation { value, .. } => {
+                    if let Some(value) = value {
+                        evalns.push(value.log(10.));
+                    }
+                }
+            }
+
+            if self.limit.map(|limit| nimpl >= limit).unwrap_or(false) {
+                break;
+            }
+        }
+
+        let stats = stats::OnlineStats::from_slice(&evalns);
+        println!(
+            "Average log10 runtime: {:.2} (± {:.2})",
+            stats.mean(),
+            stats.stddev(),
+        );
+
+        println!(
+            "Implementations: {} (avg depth: {})",
+            nimpl,
+            impld as f64 / nimpl as f64
+        );
+
+        let ((ddepth, ndead), (ddepth_size, ndead_size)) = deadinfo.iter().fold(
+            ((0, 0), (0, 0)),
+            |((ddepth, ndead), (ddepth_size, ndead_size)),
+             ((_, has_size), (depth, num))| {
+                if *has_size {
+                    ((ddepth, ndead), (ddepth_size + depth, ndead_size + num))
+                } else {
+                    ((ddepth + depth, ndead + num), (ddepth_size, ndead_size))
+                }
+            },
+        );
+
+        println!(
+            "Deadends: {} (avg depth: {})",
+            ndead + ndead_size,
+            (ddepth + ddepth_size) as f64 / (ndead + ndead_size) as f64
+        );
+
+        for ((cause, has_size), (cdepth, cnum)) in deadinfo.into_iter() {
+            println!(
+                "  - {:?} ({}): {} (avg depth: {})",
+                cause,
+                if has_size {
+                    " (with size)"
+                } else {
+                    " (without size)"
+                },
+                cnum,
+                cdepth as f64 / cnum as f64
+            );
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(StructOpt)]
+enum Command {
+    #[structopt(name = "rebuild")]
+    Rebuild(Rebuild),
+
+    #[structopt(name = "bounds")]
+    Bounds(Bounds),
+
+    #[structopt(name = "stats")]
+    Stats(Stats),
+
+    #[structopt(name = "bound")]
+    Bound(ComputeBound),
+}
+
+#[derive(StructOpt)]
+#[structopt(name = "telamon")]
+struct Opt {
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+fn main() {
+    let args = Opt::from_args();
+    env_logger::init();
+
+    let result = match &args.command {
+        Command::Rebuild(rebuild) => rebuild.run(&args),
+        Command::Bounds(bounds) => bounds.run(&args),
+        Command::Stats(stats) => stats.run(&args),
+        Command::Bound(bound) => bound.run(&args),
+    };
+
+    match result {
+        Ok(()) => (),
+        Err(err) => panic!("An error occured: {}", err),
+    }
+}
